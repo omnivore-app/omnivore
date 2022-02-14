cf6733c70f21dc9ce442522d4dfc1e55fd6b8727
Jackson Harper <jacksonh@gmail.com>
2022-02-14 09:17:58 -0800

    When fetching the library item list dont add content/originalHtml from DB

--- packages/api/src/datalayer/links/index.ts
+++ packages/api/src/datalayer/links/index.ts
@@ -51,15 +51,16 @@ const LINK_COLS = [
   'omnivore.pages.title',
   'omnivore.pages.description',
   'omnivore.pages.hash',
-  'omnivore.pages.originalHtml',
-  'omnivore.pages.content',
   'omnivore.pages.author',
   'omnivore.pages.image',
   'omnivore.pages.pageType',
   'omnivore.pages.publishedAt',
 ]
 
-const linkCols = (tx: Knex) => {
+// When fetching the library list we don't need to
+// pull all the content out of the database into
+// memory just to discard it later
+const linkColsWithoutContent = (tx: Knex) => {
   return [
     tx.raw(`
       CASE
@@ -78,6 +79,15 @@ const linkCols = (tx: Knex) => {
     ...LINK_COLS,
   ]
 }
+
+const linkCols = (tx: Knex) => {
+  return [
+    'omnivore.pages.content',
+    'omnivore.pages.originalHtml',
+    ...linkColsWithoutContent(tx),
+  ]
+}
+
 
 const readFilterQuery = (filter: ReadFilter) => {
   switch (filter) {
@@ -405,7 +415,7 @@ class UserArticleModel extends DataModel<
     const whereOperator = sort?.order === SortOrder.Ascending ? '>=' : '<='
 
     const queryPromise = tx(this.tableName)
-      .select(linkCols(tx))
+      .select(linkColsWithoutContent(tx))
       .innerJoin(Table.PAGES, 'pages.id', 'links.article_id')
       .where({ 'links.user_id': userId })
       .where(tx.raw(readFilterQuery(readFilter)))
