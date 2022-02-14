462a670932dd79f45edea1e640e537ce39cdf1d1
Hongbo Wu <hongbo@omnivore.app>
2022-02-14 13:17:18 +0800

    fix reminders router by allowing sendrid template id to be null

---
+++ packages/api/test/routers/reminders_router.test.ts
@@ -0,0 +1,56 @@
+import {
+  createTestLink,
+  createTestPage,
+  createTestReminder,
+  createTestUser,
+  deleteTestUser,
+  getReminder,
+} from '../db'
+import { request } from '../util'
+import { User } from '../../src/entity/user'
+import { Reminder } from '../../src/entity/reminder'
+import { expect } from 'chai'
+import 'mocha'
+
+describe('Reminders Router', () => {
+  const username = 'fakeUser'
+
+  let authToken: string
+  let user: User
+  let reminder: Reminder
+
+  before(async () => {
+    // create test user and login
+    user = await createTestUser(username)
+    const res = await request
+      .post('/local/debug/fake-user-login')
+      .send({ fakeEmail: user.email })
+
+    authToken = res.body.authToken
+
+    const page = await createTestPage()
+    const link = await createTestLink(user, page)
+    reminder = await createTestReminder(user, link.id)
+  })
+
+  after(async () => {
+    // clean up
+    await deleteTestUser(username)
+  })
+
+  describe('trigger reminders', () => {
+    it('should trigger reminders and update status to Complete', async () => {
+      await request
+        .post('/svc/reminders/trigger')
+        .send({
+          userId: user.id,
+          scheduleTime: reminder.remindAt,
+        })
+        .set('Authorization', `${authToken}`)
+        .expect(200)
+
+      const completed = await getReminder(reminder.id)
+      expect(completed?.status).to.eql('COMPLETED')
+    })
+  })
+})
