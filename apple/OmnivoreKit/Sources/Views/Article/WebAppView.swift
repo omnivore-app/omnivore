8d743617cb8fa321ffb973031cb067a503b10ef9
Jackson Harper <jacksonh@gmail.com>
2022-02-17 21:55:22 -0800

    Use the new verticalScrollIndicatorInsets instead of scrollIndicatorInsets

--- apple/OmnivoreKit/Sources/Views/Article/WebAppView.swift
+++ apple/OmnivoreKit/Sources/Views/Article/WebAppView.swift
@@ -35,7 +35,7 @@ import WebKit
       webView.configuration.userContentController = contentController
       webView.scrollView.delegate = context.coordinator
       webView.scrollView.contentInset.top = LinkItemDetailView.navBarHeight
-      webView.scrollView.scrollIndicatorInsets = UIEdgeInsets(top: LinkItemDetailView.navBarHeight, left: 0, bottom: 0, right: 0)
+      webView.scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: LinkItemDetailView.navBarHeight, left: 0, bottom: 0, right: 0)
 
       for action in WebViewAction.allCases {
         webView.configuration.userContentController.add(context.coordinator, name: action.rawValue)
