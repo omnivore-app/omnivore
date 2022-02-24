cf627e53cbff8b920209be58c497f492021f95e4
Satindar Dhillon <satindar@gmail.com>
2022-02-24 13:28:48 -0800

    add app specific privact and terms pages

---
+++ packages/web/pages/app/privacy.tsx
@@ -0,0 +1,5 @@
+import { PrivacyPolicy } from '../../components/templates/PrivacyPolicy'
+
+export default function Privacy(): JSX.Element {
+  return <PrivacyPolicy />
+}
---
+++ packages/web/pages/app/terms.tsx
@@ -0,0 +1,5 @@
+import { TermsAndConditions } from '../../components/templates/TermsAndConditions'
+
+export default function Terms(): JSX.Element {
+  return <TermsAndConditions />
+}
