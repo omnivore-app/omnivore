3d98604d9731ea8d20eea8361a89b6471c449508
Hongbo Wu <hongbo@omnivore.app>
2022-02-25 11:44:05 +0800

    remove labels in getArticles query

--- packages/web/lib/networking/queries/useGetLibraryItemsQuery.tsx
+++ packages/web/lib/networking/queries/useGetLibraryItemsQuery.tsx
@@ -6,7 +6,6 @@ import { articleFragment } from '../fragments/articleFragment'
 import { setLinkArchivedMutation } from '../mutations/setLinkArchivedMutation'
 import { deleteLinkMutation } from '../mutations/deleteLinkMutation'
 import { articleReadingProgressMutation } from '../mutations/articleReadingProgressMutation'
-import { labelFragment } from '../fragments/labelFragment'
 
 export type LibraryItemsQueryInput = {
   limit: number
@@ -90,9 +89,6 @@ export function useGetLibraryItemsQuery({
             cursor
             node {
               ...ArticleFields
-              labels {
-                ...LabelFields
-              }
               originalArticleUrl
             }
           }
@@ -110,7 +106,6 @@ export function useGetLibraryItemsQuery({
       }
     }
     ${articleFragment}
-    ${labelFragment}
   `
 
   const variables = {
