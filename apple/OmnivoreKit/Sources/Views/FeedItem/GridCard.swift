0d3bdeef8cc84f5a8f3b90d33ee4e90a9eca9dd6
Satindar Dhillon <satindar@gmail.com>
2022-03-11 12:08:47 -0800

    store grid/list layout preference in ios user deafults

--- apple/OmnivoreKit/Sources/Views/FeedItem/GridCard.swift
+++ apple/OmnivoreKit/Sources/Views/FeedItem/GridCard.swift
@@ -92,7 +92,7 @@ public struct GridCard: View {
 
             Menu(
               content: { contextMenuView },
-              label: { Image.dotsThree }
+              label: { Image.dotsThree.padding().background(Color.clear) }
             )
             .frame(width: 16, height: 16, alignment: .center)
             .onTapGesture { isContextMenuOpen = true }
