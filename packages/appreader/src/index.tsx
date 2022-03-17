059389bbe82d20a07cfe7125239aa94b90138f61
Jackson Harper <jacksonh@gmail.com>
2022-03-17 12:20:08 -0700

    WIP: bundled appreader for iOS
    Update webpack to bundle fonts and CSS
    
    Remove special font handling
    
    Add package.json
    
    allow setting the article data on window so it can be set in index.html
    
    Add window omnivoreArticle definition
    
    Remove unused dependencies
    
    remove unused copy-webpack plugin dep
    
    Remove unused deps
    
    Handle setting the environment dynamically from Swift
    
    Remove unused deps
    
    Remove more unused deps
    
    Update index, dont use subfolders for fonts
    
    Use a regex to replace font url patterns
    
    Remove local apple changes

---
+++ packages/appreader/src/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Box, VStack } from '@omnivore/web/components/elements/LayoutPrimitives'
+import { ArticleContainer } from '@omnivore/web/components/templates/article/ArticleContainer'
+import { ArticleAttributes } from "@omnivore/web/lib/networking/queries/useGetArticleQuery";
+import '@omnivore/web/styles/globals.css'
+import '@omnivore/web/styles/articleInnerStyling.css'
+
+const App = () => {
+  var themeId = window.localStorage.getItem('theme')
+
+  if (themeId) {
+    document.body.classList.remove(
+      'theme-default',
+      'White',
+      'Gray',
+      'LightGray',
+      'Dark'
+    )
+    document.body.classList.add(themeId)
+  }
+
+  return (
+    <>
+      <Box
+        css={{
+          overflowY: 'auto',
+          height: '100%',
+          width: '100vw',
+        }}
+      >
+        <VStack
+          alignment="center"
+          distribution="center"
+          className="disable-webkit-callout"
+        >
+          <ArticleContainer
+            viewerUsername="test"
+            article={window.omnivoreArticle as ArticleAttributes}
+            scrollElementRef={React.createRef()}
+            isAppleAppEmbed={true}
+            highlightBarDisabled={false}
+            highlightsBaseURL="https://example.com"
+            fontSize={window.fontSize ?? 18}
+            margin={0}
+          />
+        </VStack>
+      </Box>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
