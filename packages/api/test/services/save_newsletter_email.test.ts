0eb247de7f3ae7560d5abc7d9f78eb94e52c5ae3
Hongbo Wu <hongbo@omnivore.app>
2022-03-17 13:15:30 +0800

    fix elastic update script syntax error

--- packages/api/test/services/save_newsletter_email.test.ts
+++ packages/api/test/services/save_newsletter_email.test.ts
@@ -5,17 +5,25 @@ import { createTestUser, deleteTestUser } from '../db'
 import { createNewsletterEmail } from '../../src/services/newsletters'
 import { saveNewsletterEmail } from '../../src/services/save_newsletter_email'
 import { getPageByParam } from '../../src/elastic'
+import { User } from '../../src/entity/user'
+import { NewsletterEmail } from '../../src/entity/newsletter_email'
 
 describe('saveNewsletterEmail', () => {
   const username = 'fakeUser'
+
+  let user: User
+  let email: NewsletterEmail
+
+  before(async () => {
+    user = await createTestUser(username)
+    email = await createNewsletterEmail(user.id)
+  })
+
   after(async () => {
     await deleteTestUser(username)
   })
 
   it('adds the newsletter to the library', async () => {
-    const user = await createTestUser(username)
-    const email = await createNewsletterEmail(user.id)
-
     await saveNewsletterEmail({
       email: email.address,
       content: 'fake content',
@@ -33,6 +41,26 @@ describe('saveNewsletterEmail', () => {
       expect(page.title).to.equal('fake title')
       expect(page.author).to.equal('fake author')
       expect(page.content).to.contain('fake content')
+    })
+  })
+
+  it('should adds a Newsletter label to that page', async () => {
+    const newLabel = {
+      name: 'Newsletter',
+      color: '#07D2D1',
+    }
+
+    await saveNewsletterEmail({
+      email: email.address,
+      content: 'fake content 2',
+      url: 'https://example.com/2',
+      title: 'fake title',
+      author: 'fake author',
+    })
+
+    setTimeout(async () => {
+      const page = await getPageByParam({ userId: user.id })
+      expect(page?.labels).to.deep.include(newLabel)
     })
   })
 })
