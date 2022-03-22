5b07269ea3ffcdf2e75b39b6591d56be55bcf692
Hongbo Wu <hongbo@omnivore.app>
2022-03-22 14:03:38 +0800

    add tests for upload

---
+++ packages/api/test/routers/pages.test.ts
@@ -0,0 +1,23 @@
+import { request } from '../util'
+import 'mocha'
+
+describe('Pages Router', () => {
+  const token = process.env.PUBSUB_VERIFICATION_TOKEN
+
+  describe('upload', () => {
+    it('upload data to GCS', async () => {
+      const data = {
+        message: {
+          data: Buffer.from(JSON.stringify({ userId: 'userId' })).toString(
+            'base64'
+          ),
+          publishTime: new Date().toISOString(),
+        },
+      }
+      await request
+        .post(`/svc/pubsub/pages/upload/createdPage?token=${token}`)
+        .send(data)
+        .expect(200)
+    })
+  })
+})
