dfc6b4ef407ed93b14dda289756355c6c356ca9b
Hongbo Wu <hongbo@omnivore.app>
2022-03-25 11:17:22 +0800

    add a test

--- packages/api/src/resolvers/function_resolvers.ts
+++ packages/api/src/resolvers/function_resolvers.ts
@@ -306,7 +306,7 @@ export const functionResolvers = {
       }
       return article.url
     },
-    async originalArticleUrl(article: Article) {
+    async originalArticleUrl(article: { url: string }) {
       return article.url
     },
     async savedByViewer(
