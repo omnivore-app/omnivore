a43cb358a63346610c95f0f75c61e9ce40db7c5f
Hongbo Wu <hongbo@omnivore.app>
2022-03-25 11:02:13 +0800

    add a function_resolver to return originalArticleUrl from article.url

--- packages/api/src/resolvers/function_resolvers.ts
+++ packages/api/src/resolvers/function_resolvers.ts
@@ -306,6 +306,9 @@ export const functionResolvers = {
       }
       return article.url
     },
+    async originalArticleUrl(article: Article) {
+      return article.url
+    },
     async savedByViewer(
       article: { id: string; savedByViewer?: boolean },
       __: unknown,
