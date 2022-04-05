a043ab74e82784173ae6b9795048456e72f019ee
Hongbo Wu <hongbo@omnivore.app>
2022-04-05 15:49:47 +0800

    revert github action test

--- packages/api/test/resolvers/report.test.ts
+++ packages/api/test/resolvers/report.test.ts
@@ -73,7 +73,7 @@ describe('Report API', () => {
           await getRepository(ContentDisplayReport).findBy({
             elasticPageId: pageId,
           })
-        ).not.to.exist
+        ).to.exist
       })
     })
   })
