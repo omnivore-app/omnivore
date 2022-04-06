69fcff3099c4edb933102c89c088a9eeb44d85d5
Jackson Harper <jacksonh@gmail.com>
2022-04-06 14:44:04 -0700

    Remove unused theme colour

--- packages/web/components/tokens/stitches.config.ts
+++ packages/web/components/tokens/stitches.config.ts
@@ -102,7 +102,6 @@ export const { styled, css, theme, getCssText, globalCss, keyframes, config } =
       borderStyles: {},
       shadows: {
         panelShadow: '0px 4px 18px rgba(120, 123, 134, 0.12)',
-        cardItemShadow: '0px 8px 32px rgba(32, 31, 29, 0.33)',
         cardBoxShadow:
           '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)',
       },
@@ -199,7 +198,6 @@ const darkThemeSpec = {
     labelButtonsBg: '#5F5E58',
   },
   shadows: {
-    cardItemShadow: '0px 8px 32px rgba(255, 255, 255, 0.33)',
     cardBoxShadow:
       '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)',
   },
