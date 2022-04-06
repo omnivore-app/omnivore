dc028466d0c58dd3f9cf80727a9a021165b70af1
Satindar Dhillon <satindar@gmail.com>
2022-04-06 12:32:38 -0700

    resolve merge conflict

--- packages/web/components/tokens/stitches.config.ts
+++ packages/web/components/tokens/stitches.config.ts
@@ -102,7 +102,8 @@ export const { styled, css, theme, getCssText, globalCss, keyframes, config } =
       borderStyles: {},
       shadows: {
         panelShadow: '0px 4px 18px rgba(120, 123, 134, 0.12)',
-        cardBoxShadow: '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)'
+        cardBoxShadow:
+          '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)',
       },
       zIndices: {},
       transitions: {},
@@ -146,7 +147,7 @@ export const { styled, css, theme, getCssText, globalCss, keyframes, config } =
         // Avatar Fallback color
         avatarBg: '#FFFFFF',
         avatarFont: '#0A0806',
-        tooltipIcons: '#FDFAEC'
+        tooltipIcons: '#FDFAEC',
       },
     },
     media: {
@@ -193,13 +194,9 @@ const darkThemeSpec = {
     avatarFont: 'rgba(255, 255, 255, 0.8)',
   },
   shadows: {
-<<<<<<< HEAD
-    cardItemShadow: '0px 8px 32px rgba(255, 255, 255, 0.33)',
+    cardBoxShadow:
+      '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)',
   },
-=======
-    cardBoxShadow: '0px 0px 9px -2px rgba(32, 31, 29, 0.09), 0px 7px 12px rgba(32, 31, 29, 0.07)'
-  }
->>>>>>> main
 }
 
 // Avatar Fallback color
