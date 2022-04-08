32b08d11124e7db5f9f4ea94a758d348eba28edc
Jackson Harper <jacksonh@gmail.com>
2022-04-08 10:42:20 -0700

    Disable dots menu Since there are no extra actions yet

--- packages/web/components/templates/article/ArticleActionsMenu.tsx
+++ packages/web/components/templates/article/ArticleActionsMenu.tsx
@@ -7,15 +7,8 @@ import { styled, theme } from "../../tokens/stitches.config"
 export type ArticleActionsMenuLayout = 'horizontal' | 'vertical'
 
 type ArticleActionsMenuProps = {
-  // pageTestId: string
-  // hideHeader?: boolean
-  // pageMetaDataProps?: PageMetaDataProps
-  // scrollElementRef?: MutableRefObject<HTMLDivElement | null>
-  // displayFontStepper?: boolean
   layout: ArticleActionsMenuLayout
 }
-
-
 
 export function MenuSeparator(props: ArticleActionsMenuProps) {
   const LineSeparator = styled(Separator, {
@@ -33,11 +26,12 @@ export function ArticleActionsMenu(props: ArticleActionsMenuProps): JSX.Element 
     <Box
       css={{
         display: 'flex',
+
         flexDirection: props.layout == 'vertical' ? 'column' : 'row',
         alignItems: props.layout == 'vertical' ? 'flex-start' : 'center',
         justifyContent: props.layout == 'vertical' ? 'center' : 'flex-end',
+        gap: props.layout == 'vertical' ? '8px' : '4px',
         paddingTop: '6px',
-        gap: '4px',
         height: '100%',
         width: '100%',
         m: '0px',
@@ -51,6 +45,7 @@ export function ArticleActionsMenu(props: ArticleActionsMenuProps): JSX.Element 
       <Button style='articleActionIcon'>
         <TagSimple size={24} color={theme.colors.readerFont.toString()} />
       </Button>
+
       <Button style='articleActionIcon'>
         <HighlighterCircle size={24} color={theme.colors.readerFont.toString()} />
       </Button>
@@ -59,11 +54,11 @@ export function ArticleActionsMenu(props: ArticleActionsMenuProps): JSX.Element 
       <Button style='articleActionIcon'>
         <ArchiveBox size={24} color={theme.colors.readerFont.toString()} />
       </Button>
-      <MenuSeparator layout={props.layout} />
+      {/* <MenuSeparator layout={props.layout} />
 
       <Button style='articleActionIcon'>
         <DotsThree size={24} color={theme.colors.readerFont.toString()} />
-      </Button>
+      </Button> */}
     </Box>
   )
 }
