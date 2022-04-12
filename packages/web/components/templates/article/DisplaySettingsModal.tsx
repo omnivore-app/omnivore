5450c9ce298c6f47e487a20ec2734be5ae418b25
Jackson Harper <jacksonh@gmail.com>
2022-04-11 21:03:35 -0700

    Add stand alone display settings modal

---
+++ packages/web/components/templates/article/DisplaySettingsModal.tsx
@@ -0,0 +1,59 @@
+import { X } from 'phosphor-react'
+import { ArticleAttributes } from '../../../lib/networking/queries/useGetArticleQuery'
+import { UserPreferences } from '../../../lib/networking/queries/useGetUserPreferences'
+import { Button } from '../../elements/Button'
+import { CrossIcon } from '../../elements/images/CrossIcon'
+import { Box, HStack, VStack } from '../../elements/LayoutPrimitives'
+import {
+  ModalRoot,
+  ModalOverlay,
+  ModalContent,
+} from '../../elements/ModalPrimitives'
+import { StyledText } from '../../elements/StyledText'
+import { theme } from '../../tokens/stitches.config'
+import { EditLabelsControl } from './EditLabelsControl'
+import { ReaderSettings } from './ReaderSettingsModal'
+
+type DisplaySettingsModalProps = {
+  onOpenChange: (open: boolean) => void
+  userPreferences?: UserPreferences
+  articleActionHandler: (action: string, arg?: number) => void
+}
+
+export function DisplaySettingsModal(props: DisplaySettingsModalProps): JSX.Element {
+  return (
+    <ModalRoot defaultOpen onOpenChange={props.onOpenChange}>
+      <ModalOverlay />
+      <ModalContent
+        css={{ overflow: 'auto' }}
+        onPointerDownOutside={(event) => {
+          event.preventDefault()
+          props.onOpenChange(false)
+        }}
+      >
+        <VStack css={{ width: '100%' }}>
+          <HStack
+              distribution="between"
+              alignment="center"
+              css={{ width: '100%' }}
+            >
+              <StyledText style="modalHeadline" css={{ pl: '16px' }}>Labels</StyledText>
+              <Button
+                css={{ pt: '16px', pr: '16px' }}
+                style="ghost"
+                onClick={() => {
+                  props.onOpenChange(false)
+                }}
+              >
+                <CrossIcon
+                  size={14}
+                  strokeColor={theme.colors.grayText.toString()}
+                />
+              </Button>
+          </HStack>
+          <ReaderSettings {...props} />
+        </VStack>
+      </ModalContent>
+    </ModalRoot>
+  )
+}
