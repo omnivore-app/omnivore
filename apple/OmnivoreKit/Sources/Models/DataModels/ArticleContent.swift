290894ed9b7339b4937f903eb1ee1dcd50a667c5
Satindar Dhillon <satindar@gmail.com>
2022-04-17 00:04:06 -0700

    move files

--- apple/OmnivoreKit/Sources/Models/DataModels/ArticleContent.swift
+++ apple/OmnivoreKit/Sources/Models/DataModels/ArticleContent.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-public class CachedPageContent: NSObject {
-  public let slug: String
-  public let value: ArticleContent
-
-  public init(_ slug: String, _ content: ArticleContent) {
-    self.slug = slug
-    self.value = content
-  }
-}
-
 public struct ArticleContent {
   public let htmlContent: String
   public let highlights: [Highlight]
