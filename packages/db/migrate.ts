0b356c0050d47cdbd004c0561f8068d6d67d93bc
Hongbo Wu <hongbo@omnivore.app>
2022-04-29 12:06:31 +0800

    Add elastic update mappings in migration script

--- packages/db/migrate.ts
+++ packages/db/migrate.ts
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as dotenv from 'dotenv'
+import Postgrator from 'postgrator'
+import chalk from 'chalk'
+import { Client } from '@elastic/elasticsearch'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-import Postgrator from 'postgrator';
-import chalk from 'chalk';
+dotenv.config()
 
 const log = (text: string, style: typeof chalk.white = chalk.white): void =>
-  console.log(`${chalk.cyanBright('>')} ${style(text)}`);
+  console.log(`${chalk.cyanBright('>')} ${style(text)}`)
 
 interface DBEnv {
-  host: string;
-  port: number;
-  database: string;
-  username: string;
-  password: string;
+  host: string
+  port: number
+  database: string
+  username: string
+  password: string
 }
 
 const getEnv = (): DBEnv => {
@@ -23,14 +26,14 @@ const getEnv = (): DBEnv => {
     PG_DB: database,
     PG_USER: username,
     PG_PASSWORD: password,
-  } = process.env;
+  } = process.env
 
   if (typeof username !== 'string') {
-    throw new Error('No PG user passed in env');
+    throw new Error('No PG user passed in env')
   }
 
   if (typeof password !== 'string') {
-    throw new Error('No PG password passed in env');
+    throw new Error('No PG password passed in env')
   }
 
   const config = {
@@ -39,10 +42,10 @@ const getEnv = (): DBEnv => {
     database: database || 'omnivore',
     username,
     password,
-  };
+  }
 
-  return config;
-};
+  return config
+}
 
 const postgrator = new Postgrator({
   migrationDirectory: __dirname + '/migrations',
@@ -52,37 +55,75 @@ const postgrator = new Postgrator({
   schemaTable: 'schemaversion',
   // Validate migration md5 checksum to ensure the contents of the script have not changed
   validateChecksums: true,
-});
+})
 
-log('Starting migration manager');
+log('Starting migration manager')
 
-const targetMigration = process.argv[2];
+const targetMigration = process.argv[2]
 
 const targetMigrationLabel = targetMigration
   ? `'${chalk.blue(targetMigration)}'`
-  : chalk.blue('latest');
+  : chalk.blue('latest')
 
-log(`Migrating to ${targetMigrationLabel}.\n`);
+log(`Migrating to ${targetMigrationLabel}.\n`)
 
-const logAppliedMigrations = (appliedMigrations: Postgrator.Migration[]): void => {
+const logAppliedMigrations = (
+  appliedMigrations: Postgrator.Migration[]
+): void => {
   if (appliedMigrations.length > 0) {
-    log(`Applied ${chalk.green(appliedMigrations.length.toString())} migrations successfully:`);
+    log(
+      `Applied ${chalk.green(
+        appliedMigrations.length.toString()
+      )} migrations successfully:`
+    )
     for (const migration of appliedMigrations) {
-      const actionLabel = migration.action === 'do' ? chalk.green('+') : chalk.red('-');
-      console.log(`  ${actionLabel} ${migration.name}`);
+      const actionLabel =
+        migration.action === 'do' ? chalk.green('+') : chalk.red('-')
+      console.log(`  ${actionLabel} ${migration.name}`)
     }
   } else {
-    log(`No migrations applied.`);
+    log(`No migrations applied.`)
   }
-};
+}
+
+export const INDEX_ALIAS = 'pages_alias'
+export const client = new Client({
+  node: process.env.ELASTICSEARCH_URL || 'http://localhost:9200',
+  auth: {
+    username: process.env.ELASTICSEARCH_USERNAME || '',
+    password: process.env.ELASTICSEARCH_PASSWORD || '',
+  },
+})
+
+const updateMappings = async (): Promise<void> => {
+  // read index settings from file
+  const indexSettings = readFileSync(
+    join(__dirname, '..', '..', 'index_settings.json'),
+    'utf8'
+  )
+  // update mappings
+  await client.indices.putMapping({
+    index: INDEX_ALIAS,
+    body: JSON.parse(indexSettings).mappings,
+  })
+}
 
 postgrator
   .migrate(targetMigration)
   .then(logAppliedMigrations)
-  .catch(error => {
-    log(`${chalk.red('Migration failed: ')}${error.message}`, chalk.red);
-    const { appliedMigrations } = error;
-    logAppliedMigrations(appliedMigrations);
-    process.exit(1);
+  .catch((error) => {
+    log(`${chalk.red('Migration failed: ')}${error.message}`, chalk.red)
+    const { appliedMigrations } = error
+    logAppliedMigrations(appliedMigrations)
+    process.exit(1)
   })
-  .then(() => console.log('\nExiting...'));
+  .then(() => console.log('\nExiting...'))
+
+log('Starting updating elasticsearch index mappings...')
+
+updateMappings()
+  .then(() => console.log('\nExiting...'))
+  .catch((error) => {
+    log(`${chalk.red('Updating failed: ')}${error.message}`, chalk.red)
+    process.exit(1)
+  })
