fe09487a6ab56866c02763617b8b4560ced28fde
Hongbo Wu <hongbo@omnivore.app>
2022-05-25 20:46:19 +0800

    Add webhooks model

---
+++ packages/db/migrations/0083.do.webhooks.sql
@@ -0,0 +1,21 @@
+-- Type: DO
+-- Name: webhooks
+-- Description: webhooks model
+
+BEGIN;
+
+CREATE TABLE omnivore.webhooks (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+    url text NOT NULL,
+    method text NOT NULL DEFAULT 'POST',
+    content_type text NOT NULL DEFAULT 'application/json',
+    active boolean NOT NULL DEFAULT true,
+    event_types text[] NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TRIGGER update_webhook_modtime BEFORE UPDATE ON omnivore.webhooks
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+COMMIT;
