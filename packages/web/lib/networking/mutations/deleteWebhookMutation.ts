545d944ffdb40a5532c6d4a9f74d6411d1102a1a
Hongbo Wu <hongbo@omnivore.app>
2022-06-01 18:43:43 +0800

    Add get webhooks query

---
+++ packages/web/lib/networking/mutations/deleteWebhookMutation.ts
@@ -0,0 +1,39 @@
+import { gql } from 'graphql-request'
+import { gqlFetcher } from '../networkHelpers'
+import { Webhook } from '../queries/useGetWebhooksQuery'
+
+interface DeleteWebhookResult {
+  deleteWebhook: DeleteWebhook
+  errorCodes?: unknown[]
+}
+
+type DeleteWebhook = {
+  webhook: Webhook
+}
+
+export async function deleteWebhookMutation(
+  id: string
+): Promise<any | undefined> {
+  const mutation = gql`
+    mutation {
+      deleteWebhook(id: "${id}") {
+        ... on DeleteWebhookSuccess {
+          webhook {
+            id
+          }
+        }
+        ... on DeleteWebhookError {
+          errorCodes
+        }
+      }
+    }
+  `
+
+  try {
+    const data = (await gqlFetcher(mutation)) as DeleteWebhookResult
+    return data.errorCodes ? undefined : data.deleteWebhook.webhook.id
+  } catch (error) {
+    console.log('deleteWebhookMutation error', error)
+    return undefined
+  }
+}
