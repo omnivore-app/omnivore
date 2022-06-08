c86c18507e9da3d7451e6888f2342c8b0993491f
Jackson Harper <jacksonh@gmail.com>
2022-06-08 11:22:26 -0700

    Fix issue with URL normalization, add tests

--- apple/OmnivoreKit/Tests/UtilsTests/UtilsTests.swift
+++ apple/OmnivoreKit/Tests/UtilsTests/UtilsTests.swift
@@ -9,7 +9,20 @@ final class UtilsTests: XCTestCase {
     XCTAssertEqual("Hello", "Hello")
   }
 
+  func testNormalizeUrl() {
+    // trailing slash removed
+    XCTAssertEqual(normalizeURL("https://omnivore.app/"), "https://omnivore.app")
+
+    // utm_ removed
+    XCTAssertEqual(normalizeURL("https://omnivore.app/?aa=a&bb=b&utm_track=track&cc=c"), "https://omnivore.app?aa=a&bb=b&cc=c")
+
+    // query params sorted
+    XCTAssertEqual(normalizeURL("https://omnivore.app/?aa=a&cc=c&bb=b"), "https://omnivore.app?aa=a&bb=b&cc=c")
+    XCTAssertEqual(normalizeURL("https://omnivore.app/?cc=c&bb=b&aa=a"), "https://omnivore.app?aa=a&bb=b&cc=c")
+  }
+
   static var allTests = [
-    ("testExample", testExample)
+    ("testExample", testExample),
+    ("testNormalizeUrl", testNormalizeUrl)
   ]
 }
