7d7b51500c11db6d8b3236aa19b8861309deb5d3
Satindar Dhillon <satindar@gmail.com>
2022-06-30 08:04:23 -0700

    migration to allow user to delete themseleves

---
+++ packages/db/migrations/0086.undo.grant_delete_on_user_table.sql
@@ -0,0 +1,9 @@
+-- Type: UNDO
+-- Name: grant_delete_on_user_table
+-- Description: Allows the Omnivore User to delete themselves (for delete account app feature)
+
+BEGIN;
+
+-- do nothing here, there's no reason to undo this migration.
+
+COMMIT;
