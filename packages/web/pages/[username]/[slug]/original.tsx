2858dd68d2e61ac50e71854c21d6aedf5751bfd4
Jackson Harper <jacksonh@gmail.com>
2022-07-09 14:38:59 -0700

    Display in a textarea

--- packages/web/pages/[username]/[slug]/original.tsx
+++ packages/web/pages/[username]/[slug]/original.tsx
@@ -1,41 +1,5 @@
-import { PrimaryLayout } from '../../../components/templates/PrimaryLayout'
-import { LoadingView } from '../../../components/patterns/LoadingView'
-import { useGetViewerQuery } from '../../../lib/networking/queries/useGetViewerQuery'
-import { removeItemFromCache, useGetArticleQuery } from '../../../lib/networking/queries/useGetArticleQuery'
 import { useRouter } from 'next/router'
-import { Box, VStack } from './../../../components/elements/LayoutPrimitives'
-import { ArticleContainer } from './../../../components/templates/article/ArticleContainer'
-import { PdfArticleContainerProps } from './../../../components/templates/article/PdfArticleContainer'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { useKeyboardShortcuts } from '../../../lib/keyboardShortcuts/useKeyboardShortcuts'
-import { articleKeyboardCommands, navigationCommands } from '../../../lib/keyboardShortcuts/navigationShortcuts'
-import dynamic from 'next/dynamic'
-import { webBaseURL } from '../../../lib/appConfig'
-import { Toaster } from 'react-hot-toast'
-import { createHighlightMutation } from '../../../lib/networking/mutations/createHighlightMutation'
-import { deleteHighlightMutation } from '../../../lib/networking/mutations/deleteHighlightMutation'
-import { mergeHighlightMutation } from '../../../lib/networking/mutations/mergeHighlightMutation'
-import { articleReadingProgressMutation } from '../../../lib/networking/mutations/articleReadingProgressMutation'
-import { updateHighlightMutation } from '../../../lib/networking/mutations/updateHighlightMutation'
-import { userPersonalizationMutation } from '../../../lib/networking/mutations/userPersonalizationMutation'
-import Script from 'next/script'
-import { theme } from '../../../components/tokens/stitches.config'
-import { ArticleActionsMenu } from '../../../components/templates/article/ArticleActionsMenu'
-import { setLinkArchivedMutation } from '../../../lib/networking/mutations/setLinkArchivedMutation'
-import { Label } from '../../../lib/networking/fragments/labelFragment'
-import { useSWRConfig } from 'swr'
-import { showErrorToast, showSuccessToast } from '../../../lib/toastHelpers'
-import { SetLabelsModal } from '../../../components/templates/article/SetLabelsModal'
-import { DisplaySettingsModal } from '../../../components/templates/article/DisplaySettingsModal'
-import { useReaderSettings } from '../../../lib/hooks/useReaderSettings'
-import { SkeletonArticleContainer } from '../../../components/templates/article/SkeletonArticleContainer'
 import { useGetArticleOriginalHtmlQuery } from '../../../lib/networking/queries/useGetArticleOriginalHtmlQuery'
-
-
-const PdfArticleContainerNoSSR = dynamic<PdfArticleContainerProps>(
-  () => import('./../../../components/templates/article/PdfArticleContainer'),
-  { ssr: false }
-)
 
 export default function Home(): JSX.Element {
   const router = useRouter()
@@ -50,13 +14,8 @@ export default function Home(): JSX.Element {
   }
 
   return (
-    <Box
-        css={{
-          maxWidth: '100%',
-        }}
-        dangerouslySetInnerHTML={{
-          __html: originalHtml,
-        }}
-      />
+    <textarea style={{ width: '100vw', height: '100vh' }}>
+      {originalHtml}
+    </textarea>
   )
 }
