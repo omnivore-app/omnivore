d10880623418fe9069bf33351c4c6390d58eb7ec
Hongbo Wu <hongbo@omnivore.app>
2022-07-20 22:53:13 +0800

    make sender address nullable env var

--- packages/api/src/util.ts
+++ packages/api/src/util.ts
@@ -119,6 +119,9 @@ const nullableEnvVars = [
   'ELASTIC_USERNAME',
   'ELASTIC_PASSWORD',
   'GCS_UPLOAD_PRIVATE_BUCKET',
+  'SENDER_MESSAGE',
+  'SENDER_FEEDBACK',
+  'SENDER_GENERAL',
 ] // Allow some vars to be null/empty
 
 /* If not in GAE and Prod/QA/Demo env (f.e. on localhost/dev env), allow following env vars to be null */
