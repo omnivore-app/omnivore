2b240df4353800d62245a9c78156bd974a49612e
Hongbo Wu <hongbo@omnivore.app>
2022-07-21 13:33:17 +0800

    fake sendMail in sign up test

--- packages/api/src/services/create_user.ts
+++ packages/api/src/services/create_user.ts
@@ -100,7 +100,7 @@ export const createUser = async (input: {
     const confirmationLink = `${env.client.url}/confirm-email/${confirmationToken}`
     // send email
     const sent = await sendEmail({
-      from: env.sender.general,
+      from: env.sender.message,
       to: user.email,
       subject: 'Confirm your email',
       text: `Please confirm your email by clicking the link below:\n\n${confirmationLink}\n\n`,
