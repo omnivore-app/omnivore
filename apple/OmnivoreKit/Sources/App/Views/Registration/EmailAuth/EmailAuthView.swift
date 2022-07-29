8b45095466b662fa95f6010ea8ecf98c6f2a8250
Satindar Dhillon <satindar@gmail.com>
2022-07-29 11:02:36 -0700

    add username and name to sign up form for ios

---
+++ apple/OmnivoreKit/Sources/App/Views/Registration/EmailAuth/EmailAuthView.swift
@@ -0,0 +1,68 @@
+import Combine
+import Models
+import Services
+import SwiftUI
+import Utils
+import Views
+
+enum EmailAuthState {
+  case signIn
+  case signUp
+  case loading
+}
+
+@MainActor final class EmailAuthViewModel: ObservableObject {
+  @Published var loginError: LoginError?
+  @Published var emailAuthState = EmailAuthState.loading
+  @Published var potentialUsernameStatus = PotentialUsernameStatus.noUsername
+  @Published var potentialUsername = ""
+
+  var subscriptions = Set<AnyCancellable>()
+
+  func loadAuthState() {
+    // check tokens here to determine pending/active/no user
+    emailAuthState = .signIn
+  }
+}
+
+struct EmailAuthView: View {
+  @Environment(\.presentationMode) private var presentationMode
+  @StateObject private var viewModel = EmailAuthViewModel()
+
+  @ViewBuilder var primaryContent: some View {
+    switch viewModel.emailAuthState {
+    case .signUp:
+      EmailSignupFormView(viewModel: viewModel)
+    case .signIn:
+      EmailLoginFormView(viewModel: viewModel)
+    case .loading:
+      VStack {
+        Spacer()
+        ProgressView()
+        Spacer()
+      }
+    }
+  }
+
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Color.appBackground.edgesIgnoringSafeArea(.all)
+        primaryContent
+          .frame(maxWidth: 300)
+          .navigationBarTitleDisplayMode(.inline)
+          .toolbar {
+            ToolbarItem(placement: .barTrailing) {
+              Button(
+                action: { presentationMode.wrappedValue.dismiss() },
+                label: { Image(systemName: "xmark").foregroundColor(.appGrayTextContrast) }
+              )
+            }
+          }
+      }
+    }
+    .task {
+      viewModel.loadAuthState()
+    }
+  }
+}
