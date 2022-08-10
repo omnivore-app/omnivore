65a420ad8cf24014a912643ee97576b78c9867d6
Satindar Dhillon <satindar@gmail.com>
2022-08-09 19:47:15 -0700

    use retrofir to make rest api call for email login

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/LoginViewModel.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/LoginViewModel.kt
@@ -3,9 +3,25 @@ package app.omnivore.omnivore
 import android.content.ContentValues
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
   fun login(email: String, password: String) {
-    Log.v(ContentValues.TAG, "in view model $email / $password")
+    val emailLogin = RetrofitHelper.getInstance().create(EmailLoginSubmit::class.java)
+
+    GlobalScope.launch {
+      val result = emailLogin.submitEmailLogin(
+        EmailLoginCredentials(email = email, password = password)
+      )
+
+      // TODO: parse out result and store auth token
+      // set some variable that compose can observe
+      if (result != null) {
+        Log.d(ContentValues.TAG, result.body().toString())
+      } else {
+        Log.d(ContentValues.TAG, result.body().toString())
+      }
+    }
   }
 }
---
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/RESTNetworker.kt
@@ -0,0 +1,36 @@
+package app.omnivore.omnivore
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+
+data class EmailAuthPayload(
+  val authCookieString: String?,
+  val authToken: String?,
+  val pendingEmailVerification: Boolean?
+)
+
+data class EmailLoginCredentials(
+  val email: String,
+  val password: String
+)
+
+interface EmailLoginSubmit {
+  @Headers("Content-Type: application/json")
+  @POST("/api/mobile-auth/email-sign-in")
+  suspend fun submitEmailLogin(@Body credentials: EmailLoginCredentials): Response<EmailAuthPayload>
+}
+
+object RetrofitHelper {
+  private const val baseUrl = "https://api-demo.omnivore.app"
+
+  fun getInstance(): Retrofit {
+    return Retrofit.Builder().baseUrl(baseUrl)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+  }
+}
