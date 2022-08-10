f0d4c6fd7ffec0e74b70ed7fe305653f6ebc7234
Rupin Khandelwal <rupin@omnivore.app>
2022-08-10 19:00:30 +0200

    Removing the commented out code because now the functionality is supported through the Menu for Big screens

--- packages/web/components/templates/homeFeed/HomeFeedContainer.tsx
+++ packages/web/components/templates/homeFeed/HomeFeedContainer.tsx
@@ -53,21 +53,6 @@ import {
 
 export type LayoutType = 'LIST_LAYOUT' | 'GRID_LAYOUT'
 
-// TODO: remove Commented out code after PR approval (l56-l70 & 730 - 777)
-// const timeZoneHourDiff = -new Date().getTimezoneOffset() / 60
-
-// const SAVED_SEARCHES: Record<string, string> = {
-//   Inbox: `in:inbox`,
-//   'Read Later': `in:inbox -label:Newsletter`,
-//   Highlights: `type:highlights`,
-//   Today: `in:inbox saved:${
-//     new Date(new Date().getTime() - 24 * 3600000).toISOString().split('T')[0]
-//   }Z${timeZoneHourDiff.toLocaleString('en-US', {
-//     signDisplay: 'always',
-//   })}..*`,
-//   Newsletters: `in:inbox label:Newsletter`,
-// }
-
 const fetchSearchResults = async (query: string, cb: any) => {
   if (!query.startsWith('#')) return
   const res = await typeaheadSearchQuery({
@@ -727,54 +712,6 @@ function HomeFeedGrid(props: HomeFeedContentProps): JSX.Element {
           searchTerm={props.searchTerm}
           applySearchQuery={props.applySearchQuery}
         />
-        { //viewerData?.me && (
-          // <Box
-          //   css={{
-          //     display: 'flex',
-          //     width: '100%',
-          //     height: '44px',
-          //     marginTop: '16px',
-          //     gap: '8px',
-          //     flexDirection: 'row',
-          //     overflowY: 'scroll',
-          //     scrollbarWidth: 'none',
-          //     '&::-webkit-scrollbar': {
-          //       display: 'none',
-          //     },
-          //   }}
-          // >
-          //   {Object.keys(SAVED_SEARCHES).map((key) => {
-          //     const isInboxTerm = (term: string) => {
-          //       return !term || term === 'in:inbox'
-          //     }
-
-          //     const searchQuery = SAVED_SEARCHES[key]
-          //     const style =
-          //       searchQuery === props.searchTerm ||
-          //       (!props.searchTerm && isInboxTerm(searchQuery))
-          //         ? 'ctaDarkYellow'
-          //         : 'ctaLightGray'
-          //     return (
-          //       <Button
-          //         key={key}
-          //         style={style}
-          //         onClick={() => {
-          //           props.applySearchQuery(searchQuery)
-          //         }}
-          //         css={{
-          //           p: '10px 12px',
-          //           height: '37.5px',
-          //           borderRadius: '6px',
-          //           whiteSpace: 'nowrap',
-          //         }}
-          //       >
-          //         {key}
-          //       </Button>
-          //     )
-          //   })}
-          // </Box>
-          //)
-        }
 
         {!props.isValidating && props.items.length == 0 ? (
           <EmptyLibrary
