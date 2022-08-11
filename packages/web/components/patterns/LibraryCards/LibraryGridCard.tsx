695a9ce649ed49c4096e41e5d89475208b6baa1b
Jackson Harper <jacksonh@gmail.com>
2022-08-11 17:36:09 +0800

    Add page with new library layout

---
+++ packages/web/components/patterns/LibraryCards/LibraryGridCard.tsx
@@ -0,0 +1,178 @@
+import { Box, VStack, HStack, SpanBox } from '../../elements/LayoutPrimitives'
+import { CoverImage } from '../../elements/CoverImage'
+import { StyledText } from '../../elements/StyledText'
+import { authoredByText } from '../ArticleSubtitle'
+import { MoreOptionsIcon } from '../../elements/images/MoreOptionsIcon'
+import { theme } from '../../tokens/stitches.config'
+import { CardMenu } from '../CardMenu'
+import { LabelChip } from '../../elements/LabelChip'
+import { ProgressBar } from '../../elements/ProgressBar'
+import type { LinkedItemCardProps } from './CardTypes'
+
+export function LibraryGridCard(props: LinkedItemCardProps): JSX.Element {
+  return (
+    <VStack
+      css={{
+        p: '12px', 
+        height: '100%',
+        width: '100%',
+        maxWidth: '100%',
+        borderRadius: '8px',
+        cursor: 'pointer',
+        wordBreak: 'break-word',
+        overflow: 'clip',
+        border: '1px solid $border',
+        position: 'relative',
+      }}
+      alignment="start"
+      distribution="start"
+      onClick={() => {
+        props.handleAction('showDetail')
+      }}
+    >
+      {props.item.image && (
+        <CoverImage
+          src={props.item.image}
+          alt="Link Preview Image"
+          width="100%"
+          height={160}
+          css={{ borderRadius: '8px' }}
+          onError={(e) => {
+            ;(e.target as HTMLElement).style.display = 'none'
+          }}
+        />
+      )}
+      <VStack
+        distribution="start"
+        alignment="start"
+        css={{
+          px: '0px',
+          width: '100%',
+          pl: '$1',
+        }}
+      >
+        <HStack
+          alignment="start"
+          distribution="between"
+          css={{
+            width: '100%',
+            p: '0px',
+            mr: '-12px',
+            mt: '15px',
+            display: 'grid',
+            gridTemplateColumns: '1fr 24px',
+            gridTemplateRows: '1fr',
+          }}
+        >
+          <CardTitle title={props.item.title} />
+          {/* <Box
+            css={{ alignSelf: 'end', alignItems: 'start', height: '100%' }}
+            onClick={(e) => {
+              // This is here to prevent menu click events from bubbling
+              // up and causing us to "click" on the link item.
+              e.stopPropagation()
+            }}
+          >
+            <CardMenu
+              item={props.item}
+              viewer={props.viewer}
+              triggerElement={
+                <MoreOptionsIcon
+                  size={24}
+                  strokeColor={theme.colors.grayTextContrast.toString()}
+                  orientation="horizontal"
+                />
+              }
+              actionHandler={props.handleAction}
+            />
+          </Box> */}
+        </HStack>
+        {/* <HStack alignment="start" distribution="between">
+          <StyledText style="caption" css={{ my: '0', mt: '-$2' }}>
+            {props.item.author && (
+              <SpanBox css={{ mr: '8px' }}>
+                {authoredByText(props.item.author)}
+              </SpanBox>
+            )}
+            <SpanBox css={{ textDecorationLine: 'underline' }}>
+              {props.originText}
+            </SpanBox>
+          </StyledText>
+        </HStack> */}
+      </VStack>
+      <HStack
+        alignment="start"
+        distribution="between"
+        css={{
+          width: '100%',
+          pt: '$2',
+          px: '$1',
+          pr: '12px',
+          mt: '7px',
+          flexGrow: '1',
+        }}
+      >
+        <StyledText
+          css={{
+            m: 0,
+            py: '0px',
+            mr: '$2',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontSize: '14px',
+            lineHeight: '125%',
+            color: '$grayTextContrast',
+            flexGrow: '4',
+            overflow: 'hidden',
+            display: '-webkit-box',
+            WebkitLineClamp: 5,
+            WebkitBoxOrient: 'vertical',
+          }}
+          data-testid="listDesc"
+        >
+          {props.item.description}
+        </StyledText>
+
+      </HStack>
+      <Box css={{ display: 'block', py: '12px' }}>
+        {props.item.labels?.map(({ name, color }, index) => (
+          <LabelChip key={index} text={name || ''} color={color} />
+        ))}
+      </Box>
+      <ProgressBar
+          fillPercentage={props.item.readingProgressPercent}
+          fillColor={theme.colors.highlight.toString()}
+          backgroundColor={theme.colors.lightBorder.toString()}
+          borderRadius={
+            props.item.readingProgressPercent === 100 ? '0' : '0px 8px 8px 0px'
+          }
+        />
+    </VStack>
+  )
+}
+
+type CardTitleProps = {
+  title: string
+}
+
+function CardTitle(props: CardTitleProps): JSX.Element {
+  return (
+    <StyledText
+      style="listTitle"
+      data-testid="listTitle"
+      css={{
+        mt: '0',
+        mb: '0',
+        fontSize: '18px',
+        textAlign: 'left',
+        lineHeight: '1.25',
+        // whiteSpace: 'nowrap',
+        // textOverflow: 'ellipsis',
+        width: '100%',
+        // overflow: 'hidden',
+      }}
+    >
+      {props.title}
+    </StyledText>
+  )
+}
---
+++ packages/web/components/templates/library/LibraryList.tsx
@@ -0,0 +1,114 @@
+import { Box } from '../../elements/LayoutPrimitives'
+import { useGetViewerQuery } from '../../../lib/networking/queries/useGetViewerQuery'
+import { useGetUserPreferences } from '../../../lib/networking/queries/useGetUserPreferences'
+import { useMemo, useState } from 'react'
+import { useGetLibraryItemsQuery } from '../../../lib/networking/queries/useGetLibraryItemsQuery'
+import { LinkedItemCardAction } from '../../patterns/LibraryCards/CardTypes'
+import { LibraryGridCard } from '../../patterns/LibraryCards/LibraryGridCard'
+
+export type LayoutType = 'LIST_LAYOUT' | 'GRID_LAYOUT'
+
+
+export function LibraryList(): JSX.Element {
+  useGetUserPreferences()
+
+  const [layout, setLayout] = useState<LayoutType>('GRID_LAYOUT')
+  const { viewerData } = useGetViewerQuery()
+
+
+  const defaultQuery = {
+    limit: 50,
+    sortDescending: true,
+    searchQuery: undefined,
+  }
+
+  const { itemsPages, size, setSize, isValidating, performActionOnItem } =
+    useGetLibraryItemsQuery(defaultQuery)
+
+  const libraryItems = useMemo(() => {
+    const items =
+      itemsPages?.flatMap((ad) => {
+        return ad.search.edges
+      }) || []
+    return items
+  }, [itemsPages, performActionOnItem])
+
+  return (
+    <Box css={{ overflowY: 'scroll' }}>
+    {/* // {!isValidating && items.length == 0 ? (
+    //   <EmptyLibrary
+    //     onAddLinkClicked={() => {
+          
+    //     }}
+    //   />
+    // ) : ( */}
+      <Box
+        css={{
+          display: 'grid',
+          gridGap: layout == 'LIST_LAYOUT' ? '0' : '16px',
+          width: '100%',
+          gridAutoRows: 'auto',
+          borderRadius: '8px',
+          marginBottom: '0px',
+          paddingRight: '14px',
+          paddingTop: '0px',
+          marginTop: layout == 'LIST_LAYOUT' ? '21px' : '0',
+          paddingBottom: layout == 'LIST_LAYOUT' ? '0px' : '21px',
+          '@smDown': {
+            border: 'unset',
+            width: layout == 'LIST_LAYOUT' ? '100vw' : undefined,
+            margin: layout == 'LIST_LAYOUT' ? '16px -16px' : undefined,
+            borderRadius: layout == 'LIST_LAYOUT' ? 0 : undefined,
+          },
+          '@lg': {
+            gridTemplateColumns:
+            layout == 'LIST_LAYOUT' ? 'none' : '1fr 1fr',
+          },
+          '@xl': {
+            gridTemplateColumns:
+              layout == 'LIST_LAYOUT' ? 'none' : 'repeat(3, 1fr)',
+          },
+        }}
+      >
+        {libraryItems.map((linkedItem) => (
+          <Box
+            className="linkedItemCard"
+            data-testid="linkedItemCard"
+            id={linkedItem.node.id}
+            tabIndex={0}
+            key={linkedItem.node.id}
+            css={{
+              width: '100%',
+              '&> div': {
+                bg: '$libraryBackground',
+              },
+              '&:focus': {
+                '> div': {
+                  bg: '$grayBgActive',
+                },
+              },
+              '&:hover': {
+                '> div': {
+                  bg: '$grayBgActive',
+                },
+              },
+            }}
+          >
+            {viewerData?.me && (
+              <LibraryGridCard
+                layout={layout}
+                item={linkedItem.node}
+                viewer={viewerData.me}
+                handleAction={(action: LinkedItemCardAction) => {
+                  
+                }}
+              />
+            )}
+          </Box>
+        ))}
+      </Box>
+        {/* Extra padding at bottom to give space for scrolling */}
+        <Box css={{ width: '100%', height: '200px' }} />
+      </Box>
+  )
+}
