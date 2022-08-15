bd89ee4e177de837bcfe0ac4d2926974281f2b4d
Jackson Harper <jacksonh@gmail.com>
2022-08-15 14:44:05 +0800

    Handle changing the list layout and search term using a coordinator pattern

--- packages/web/components/templates/library/LibraryContainer.tsx
+++ packages/web/components/templates/library/LibraryContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, SpanBox, VStack } from './../../elements/LayoutPrimitives'
+import { HStack, SpanBox, VStack } from './../../elements/LayoutPrimitives'
 import { useGetViewerQuery } from '../../../lib/networking/queries/useGetViewerQuery'
 import { useGetUserPreferences } from '../../../lib/networking/queries/useGetUserPreferences'
 import { LibraryMenu } from './LibraryMenu'
@@ -6,19 +6,58 @@ import { LibraryAvatar } from './LibraryAvatar'
 import { LibrarySearchBar } from './LibrarySearchBar'
 import { LibraryList } from './LibraryList'
 import { LibraryHeadline } from './LibraryHeadline'
+import { useCallback, useState } from 'react'
+import { LibraryItemsQueryInput } from '../../../lib/networking/queries/useGetLibraryItemsQuery'
+import { usePersistedState } from '../../../lib/hooks/usePersistedState'
 
+
+
+export type SearchCoordinator = {
+  applySearch: (searchTerm: string) => void
+}
+
+const useSearchCoordinator = () => {
+  const applySearch = useCallback((searchTerm: string) => {
+
+  }, [])
+
+  return {
+    applySearch
+  }
+}
+
+export type LibraryLayoutType = 'LIST_LAYOUT' | 'GRID_LAYOUT'
+
+export type LayoutCoordinator = {
+  layout: LibraryLayoutType
+  setLayout: (type: LibraryLayoutType) => void
+}
+
+const useLibraryLayoutCoordinator = () => {
+  const [layout, setLayout] = usePersistedState<LibraryLayoutType>({
+    key: 'libraryLayout',
+    initialValue: 'GRID_LAYOUT',
+  })
+
+  return {
+    layout,
+    setLayout
+  }
+}
 
 export function LibraryContainer(): JSX.Element {
   useGetUserPreferences()
 
   const { viewerData } = useGetViewerQuery()
+  const searchCoordinator = useSearchCoordinator()
+  const layoutCoordinator = useLibraryLayoutCoordinator()
 
   return (
     <>
       <VStack alignment="start" distribution="start" css={{ width: '100vw', height: '100vh', overflow: 'hidden', bg: '$libraryBackground' }}>
         <HStack alignment="start" distribution="start" css={{ width: '100%' }}>
           <SpanBox css={{ width: '100%', height: '90px' }}>
-            <LibrarySearchBar />
+            <LibrarySearchBar coordinator={searchCoordinator} />
           </SpanBox>
           <SpanBox css={{ marginLeft: 'auto', width: '130px', height: '100%' }}>
             <LibraryAvatar viewer={viewerData?.me} />
@@ -27,8 +66,8 @@ export function LibraryContainer(): JSX.Element {
         <HStack alignment="start" distribution="start" css={{ width: '100%', height: '100%' }}>
           <LibraryMenu />
           <VStack alignment="start" distribution="start" css={{ width: '100%', height: '100%', mr: '20px' }}>
-            <LibraryHeadline />
-            <LibraryList />
+            <LibraryHeadline layoutCoordinator={layoutCoordinator} />
+            <LibraryList layoutCoordinator={layoutCoordinator} />
           </VStack>
         </HStack>
       </VStack>
--- packages/web/components/templates/library/LibraryHeadline.tsx
+++ packages/web/components/templates/library/LibraryHeadline.tsx
@@ -5,21 +5,33 @@ import { theme } from '../../tokens/stitches.config'
 import { LibraryListLayoutIcon } from '../../elements/images/LibraryListLayoutIcon'
 import { LibraryGridLayoutIcon } from '../../elements/images/LibraryGridLayoutIcon'
 import { Button } from '../../elements/Button'
+import { LayoutCoordinator, LibraryLayoutType } from './LibraryContainer'
+import { useCallback } from 'react'
 
+export type LibraryHeadlineProps = {
+  layoutCoordinator: LayoutCoordinator
+}
 
-export function LibraryHeadline(): JSX.Element {
-  useGetUserPreferences()
+export function LibraryHeadline(props: LibraryHeadlineProps): JSX.Element {
+
+  const typeColor = useCallback((type: LibraryLayoutType) => {
+    return (
+      props.layoutCoordinator.layout === type
+      ? theme.colors.omnivoreCtaYellow.toString()
+      : "#D6D6D6"
+    )
+  }, [props.layoutCoordinator.layout])
 
   return (
     <HStack alignment="center" distribution="start" css={{ pt: '4px', pb: '8px', width: '100%', pr: '15px' }}>
       <StyledText style="libraryHeader">Home</StyledText>
       <HStack alignment="center" distribution="start" css={{ marginLeft: 'auto', gap: '16px' }}>
         <Button style="ctaDarkYellow">Add Link</Button>
-        <Button style="ghost">
-          <LibraryListLayoutIcon color="#D6D6D6" />
+        <Button style="ghost" onClick={() => props.layoutCoordinator.setLayout('LIST_LAYOUT')}>
+          <LibraryListLayoutIcon color={typeColor('LIST_LAYOUT')} />
         </Button>
-        <Button style="ghost">
-          <LibraryGridLayoutIcon color={theme.colors.omnivoreCtaYellow.toString()} />
+        <Button style="ghost" onClick={() => props.layoutCoordinator.setLayout('GRID_LAYOUT')}>
+          <LibraryGridLayoutIcon color={typeColor('GRID_LAYOUT')} />
         </Button>
       </HStack>
     </HStack>
