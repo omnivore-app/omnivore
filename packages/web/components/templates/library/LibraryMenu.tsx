7a19d613aca04a23602053d9d77ba3f2a0a8e487
Rupin Khandelwal <rupin@omnivore.app>
2022-08-17 16:12:21 -0500

    Added CSS to make Menu scrollable

--- packages/web/components/templates/library/LibraryMenu.tsx
+++ packages/web/components/templates/library/LibraryMenu.tsx
@@ -2,12 +2,20 @@ import { VStack } from '../../elements/LayoutPrimitives'
 import { useGetUserPreferences } from '../../../lib/networking/queries/useGetUserPreferences'
 import { Menubar } from '../Menu'
 
-
 export function LibraryMenu(): JSX.Element {
   useGetUserPreferences()
 
   return (
-    <VStack alignment="start" distribution="start" css={{ width: '286px', pl: '16px', height: '100%' }}>
+    <VStack
+      alignment="start"
+      distribution="start"
+      css={{
+        width: '286px',
+        minWidth: '286px',
+        height: 'calc(100% - 100px)',
+        overflowY: 'auto',
+      }}
+    >
       <Menubar />
     </VStack>
   )
