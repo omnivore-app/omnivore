0a9d4a472f05661fd32c09b34f94ae3189cd545c
Rupin Khandelwal <rupin@omnivore.app>
2022-08-18 16:55:04 -0500

    Fix/1092 - Semi Bold font for Menu Items

--- packages/web/components/templates/library/LibraryMenu.tsx
+++ packages/web/components/templates/library/LibraryMenu.tsx
@@ -14,6 +14,7 @@ export function LibraryMenu(): JSX.Element {
         minWidth: '286px',
         height: 'calc(100% - 100px)',
         overflowY: 'auto',
+        fontWeight: '600',
       }}
     >
       <Menubar />
