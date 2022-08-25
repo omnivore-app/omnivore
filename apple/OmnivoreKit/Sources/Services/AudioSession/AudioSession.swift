169149b3e123bc18288412f29a4abf67e4625cc6
Jackson Harper <jacksonh@gmail.com>
2022-08-25 20:11:39 +0800

    Dont force unwrap author and title

--- apple/OmnivoreKit/Sources/Services/AudioSession/AudioSession.swift
+++ apple/OmnivoreKit/Sources/Services/AudioSession/AudioSession.swift
@@ -296,8 +296,8 @@ public class AudioSession: NSObject, ObservableObject, AVAudioPlayerDelegate {
 
     if let item = item {
       MPNowPlayingInfoCenter.default().nowPlayingInfo = [
-        MPMediaItemPropertyTitle: NSString(string: item.title!),
-        MPMediaItemPropertyArtist: NSString(string: item.author!),
+        MPMediaItemPropertyTitle: NSString(string: item.title ?? "Your Omnivore Article"),
+        MPMediaItemPropertyArtist: NSString(string: item.author ?? "Omnivore"),
         MPMediaItemPropertyPlaybackDuration: NSNumber(value: duration),
         MPNowPlayingInfoPropertyElapsedPlaybackTime: NSNumber(value: timeElapsed)
       ]
