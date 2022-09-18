4b84215970788bff76f0ab287914c467d87a69ca
Jackson Harper <jacksonh@gmail.com>
2022-09-18 13:39:49 +0800

    Set an annotation preset to override the default opacity when creating highlights
    For some reason this was not being set on Safari.

--- packages/web/components/templates/article/PdfArticleContainer.tsx
+++ packages/web/components/templates/article/PdfArticleContainer.tsx
@@ -131,9 +131,17 @@ export default function PdfArticleContainer(
         return [copy, remove]
       }
 
+      const annotationPresets = PSPDFKit.defaultAnnotationPresets;
+      annotationPresets.highlight = {
+        opacity: 0.45,
+        color: new PSPDFKit.Color({ r: 255, g: 210, b: 52 }),
+        blendMode: PSPDFKit.BlendMode.multiply,
+      };
+
       instance = await PSPDFKit.load({
         container: container || '.pdf-container',
         toolbarItems,
+        annotationPresets,
         document: props.article.url,
         theme: isDarkTheme() ? PSPDFKit.Theme.DARK : PSPDFKit.Theme.LIGHT,
         baseUrl: `${window.location.protocol}//${window.location.host}/`,
@@ -145,6 +153,8 @@ export default function PdfArticleContainer(
           currentPageIndex: props.article.readingProgressAnchorIndex || 0,
         }),
       })
+
+      console.log('toolbars: ', instance.toolbarItems, instance.currentAnnotationPreset, instance.annotationPresets)
 
       instance.addEventListener('annotations.willChange', (event) => {
         const annotation = event.annotations.get(0)
@@ -275,7 +285,7 @@ export default function PdfArticleContainer(
             const annotation = new PSPDFKit.Annotations.HighlightAnnotation({
               pageIndex: highlightAnnotation.pageIndex,
               rects: rects,
-              opacity: 0.2,
+              opacity: 0.4,
               boundingBox: PSPDFKit.Geometry.Rect.union(rects),
               customData: {
                 omnivoreHighlight: {
