cd13e5874a20f6d0619cf9062880b5eb096ffae5
Hongbo Wu <hongbo@omnivore.app>
2022-10-10 17:44:55 +0800

    Create search_history table and entity class

---
+++ packages/api/src/entity/search_history.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { User } from './user'
+
+@Entity({ name: 'search_history' })
+export class SearchHistory {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user!: User
+
+  @Column('varchar', { length: 255 })
+  keyword!: string
+
+  @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+  createdAt!: Date
+
+  @UpdateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt!: Date
+}
---
+++ packages/db/migrations/0097.do.search_history.sql
@@ -0,0 +1,17 @@
+-- Type: DO
+-- Name: search_history
+-- Description: Create search_history table which contains searched keyword and timestamp
+
+BEGIN;
+
+CREATE TABLE omnivore.search_history (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+    user_id uuid NOT NULL REFERENCES omnivore.user ON DELETE CASCADE,
+    keyword VARCHAR(255) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TRIGGER search_history_modtime BEFORE UPDATE ON omnivore.search_history FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+COMMIT;
