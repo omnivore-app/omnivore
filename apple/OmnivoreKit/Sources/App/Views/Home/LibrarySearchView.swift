015167af602dc2db8731a97f232e379bcf1a8549
Jackson Harper <jacksonh@gmail.com>
2022-10-11 11:33:53 +0800

    Remove old code

--- apple/OmnivoreKit/Sources/App/Views/Home/LibrarySearchView.swift
+++ apple/OmnivoreKit/Sources/App/Views/Home/LibrarySearchView.swift
@@ -141,12 +141,3 @@ struct LibrarySearchView: View {
     }
   }
 }
-
-extension AnyTransition {
-  static var moveAndFade: AnyTransition {
-    .asymmetric(
-      insertion: .move(edge: .trailing).combined(with: .opacity),
-      removal: .scale.combined(with: .opacity)
-    )
-  }
-}
