fcef8e14e7142189fc9bc3bdb7c0c1e04f9ff928
Satindar Dhillon <satindar@gmail.com>
2022-10-14 13:44:04 -0700

    make sure analytics is setup before calling show on Intercom

--- packages/web/pages/support.tsx
+++ packages/web/pages/support.tsx
@@ -1,10 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
 import { SettingsLayout } from '../components/templates/SettingsLayout'
+import { setupAnalytics } from '../lib/analytics'
 
 export default function Support(): JSX.Element {
-  useEffect(() => {
+  const initAnalytics = useCallback(() => {
+    setupAnalytics()
     window.Intercom('show')
   }, [])
+
+  useEffect(() => {
+    initAnalytics()
+    window.addEventListener('load', initAnalytics)
+    return () => {
+      window.removeEventListener('load', initAnalytics)
+    }
+  }, [initAnalytics])
 
   return (
     <SettingsLayout title="Support">
