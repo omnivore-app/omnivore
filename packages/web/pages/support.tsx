6a063acb23a04f6d5b95882410d5033644828cf1
Satindar Dhillon <satindar@gmail.com>
2022-10-14 15:01:02 -0700

    add button to open chat in case it fails to load initially

--- packages/web/pages/support.tsx
+++ packages/web/pages/support.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useCallback } from 'react'
+import { Button } from '../components/elements/Button'
+import { HStack } from '../components/elements/LayoutPrimitives'
 import { SettingsLayout } from '../components/templates/SettingsLayout'
 import { setupAnalytics } from '../lib/analytics'
 
@@ -17,7 +19,33 @@ export default function Support(): JSX.Element {
 
   return (
     <SettingsLayout title="Support">
-      <></>
+      <HStack
+        alignment="center"
+        distribution="end"
+        css={{
+          pr: '$3',
+          height: '80px',
+          'input:focus': {
+            outline: '5px auto -webkit-focus-ring-color',
+          },
+          'button:focus': {
+            outline: '5px auto -webkit-focus-ring-color',
+          },
+        }}
+      >
+        <Button
+          style={'ctaOutlineYellow'}
+          type="button"
+          onClick={(event) => {
+            event.preventDefault()
+            if (window.Intercom) {
+              window.Intercom('show')
+            }
+          }}
+        >
+          {'Open Chat Window'}
+        </Button>
+      </HStack>
     </SettingsLayout>
   )
 }
