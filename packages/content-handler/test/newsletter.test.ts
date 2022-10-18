57846a1c5e5ed036daa19821eb4b05b32267574a
Hongbo Wu <hongbo@omnivore.app>
2022-10-18 15:14:38 +0800

    Fix tests

--- packages/content-handler/test/newsletter.test.ts
+++ packages/content-handler/test/newsletter.test.ts
@@ -201,16 +201,13 @@ describe('Newsletter email test', () => {
           .head(
             '/c/eJxNkk2TojAQhn-N3KTyQfg4cGDGchdnYcsZx9K5UCE0EMVAkTiKv36iHnarupNUd7rfVJ4W3EDTj1M89No496Uw0wCxgovuwBgYnbOGsZBVjDHzKPWYU8VehUMWOlIX9Qhw4rKLzXgGZziXnRTcyF7dK0iIGMVOG_OS1aTmKPRDilgVhTQUPCQIcE0x-MFTmJ8rCUpA3KtuenR2urg1ZtAzmszI0tq_Z7m66y-ilQo0uAqMTQ7WRX8auJKg56blZg7WB-iHDuYEBzO6NP0R1IwuYFphQbbTjnTH9NBfs80nym4Zyj8uUvyKbtUyGr5eUz9fNDQ7JCxfJDo9dW1lY9lmj_JNivPbGmf2Pt_lN9tDit9b-WeTetni85Z9pDpVOd7L1E_Vy7egayNO23ZP34eSeLJeux1b0rer_xaZ7ykS78nuSjMY-nL98rparNZNcv07JCjN06_EkTFBxBqOUMACErnELUNMSxTUjLDQZwzcqa4bRjCfeejUEFefS224OLr2S5wxPtij7lVrs80d2CNseRV2P52VNFMBipcdVE-U5jkRD7hFAwpGOylVwU2Mfc9qBh7DoR89yVnWXhgQFHnIsbpVb6tU_B-hH_2yzWY'
           )
-          .reply(302, undefined, {
+          .reply(301, undefined, {
             Location:
               'https://newsletter.slowchinese.net/p/companies-that-eat-people-217',
           })
         nock('https://newsletter.slowchinese.net')
           .head('/p/companies-that-eat-people-217')
           .reply(200, '')
-      })
-      after(() => {
-        nock.restore()
       })
 
       it('gets the URL from the header', async () => {
@@ -229,15 +226,12 @@ describe('Newsletter email test', () => {
           .head(
             '/ss/c/AX1lEgEQaxtvFxLaVo0GBo_geajNrlI1TGeIcmMViR3pL3fEDZnbbkoeKcaY62QZk0KPFudUiUXc_uMLerV4nA/3k5/3TFZmreTR0qKSCgowABnVg/h30/zzLik7UXd1H_n4oyd5W8Xu639AYQQB2UXz-CsssSnno'
           )
-          .reply(302, undefined, {
+          .reply(301, undefined, {
             Location: 'https://www.milkroad.com/p/talked-guy-spent-30m-beeple',
           })
-          .get('/p/talked-guy-spent-30m-beeple')
+        nock('https://www.milkroad.com')
+          .head('/p/talked-guy-spent-30m-beeple')
           .reply(200, '')
-      })
-
-      after(() => {
-        nock.restore()
       })
 
       it('gets the URL from the header', async () => {
@@ -246,7 +240,7 @@ describe('Newsletter email test', () => {
         expect(url).to.startWith(
           'https://www.milkroad.com/p/talked-guy-spent-30m-beeple'
         )
-      }).timeout(10000)
+      })
     })
 
     context('when email is from convertkit', () => {
@@ -255,22 +249,19 @@ describe('Newsletter email test', () => {
           .head(
             '/ls/click?upn=MnmHBiCwIPe9TmIJeskmA7wDTJvdVU1ACmSJ753YuhScf71JWthxqM8RnVh-2FZG0rYzrbR04P99S2ld2OkTtQmrx2FDwArpYdk5N0jVpN9dLBZ-2BdPNqkRHxNvuygY8-2F-2FtRNFoPjxjtTuyWM6L3tcYDYnAnL2xCueddWcFlUNrQWsvLotmgvC-2BrQc7bxsZhW0pUBmS_vVXscVLXlj5UtQe3aqo5RMTdTq2PepdZjP86UOmA8nzSBnnaDN-2FNHWDodWnbUOPZ063v3w3z8QtcaPpE1qNu8xYkNJJFb-2F1uZEG-2BzsLfyDkjvvVX5zYs5OyyRYlhMOlXDJcr4-2FtMrFwii0uFAvwbhxDdnTxEpi-2F7maufyH39AEO-2BtCeSUg5V4FM43UpI1zUSXeWK-2Fh5JumSmR5XhrrRAig-3D-3D'
           )
-          .reply(302, undefined, {
+          .reply(301, undefined, {
             Location: 'https://fs.blog/brain-food/october-16-2022/',
           })
-          .get('/brain-food/october-16-2022/')
+        nock('https://fs.blog')
+          .head('/brain-food/october-16-2022/')
           .reply(200, '')
-      })
-
-      after(() => {
-        nock.restore()
       })
 
       it('gets the URL from the header', async () => {
         const html = load('./test/data/convertkit-newsletter.html')
         const url = await new ConvertkitHandler().findNewsletterUrl(html)
         expect(url).to.startWith('https://fs.blog/brain-food/october-16-2022/')
-      }).timeout(10000)
+      })
     })
 
     it('returns undefined if it is not a newsletter', async () => {
@@ -285,22 +276,17 @@ describe('Newsletter email test', () => {
           .head(
             '/ls/click?upn=MnmHBiCwIPe9TmIJeskmA9nRLefEmmgrd5xWS-2Bc39wxPBpwDRny1FmWt1H0FpgKAz1dv_vVXscVLXlj5UtQe3aqo5RMTdTq2PepdZjP86UOmA8nzulL-2F3YyC-2FHgJV0JnOPtjNvgjHSaQVfisQ15hPQtnlo4t73zgTQL4QnDoer4qJ3-2F2Lf-2F2ElFMF3NyoUD4eqWCWwUM0w4P9Feaeo-2BolkySAB611BySXRt6V3Z-2F7mQcpcRX3D9zV-2B-2FdRY0Vn30aR-2BKY8qpTFuivxzF19UkQGjK5srg-3D-3D'
           )
-          .reply(302, undefined, {
+          .reply(301, undefined, {
             Location: 'https://www.openml.fyi/2022-10-14/',
           })
-          .get('/2022-10-14/')
-          .reply(200, '')
-      })
-
-      after(() => {
-        nock.restore()
+        nock('https://www.openml.fyi').head('/2022-10-14/').reply(200, '')
       })
 
       it('gets the URL from the header', async () => {
         const html = load('./test/data/ghost-newsletter.html')
         const url = await new GhostHandler().findNewsletterUrl(html)
         expect(url).to.startWith('https://www.openml.fyi/2022-10-14/')
-      }).timeout(10000)
+      })
     })
 
     context('when email is from cooper press', () => {
@@ -309,22 +295,17 @@ describe('Newsletter email test', () => {
           .head(
             '/ls/click?upn=MnmHBiCwIPe9TmIJeskmA7mFdqmsIs-2B5Xs-2FNpSIs56o0z9xhskaXR4aYohHPLtwRHfml_vVXscVLXlj5UtQe3aqo5RMTdTq2PepdZjP86UOmA8nxtQVfuqJiLh7Fio3fEtt5ouN4IH56AfszUQpxY-2FQ233kp0bjSZhBBVWAB43dgKumQkDW-2BxDFnQIUpvhmEgzSJq-2FMRG00GM7fkZVuPU-2BX8cdg8AGRHUU9Qhw6W67XEMkJVygTdm70Mo9ypNi8N33hgmhM3F6un9s7p1K1Gq-2FunslA-3D-3D'
           )
-          .reply(302, undefined, {
+          .reply(301, undefined, {
             Location: 'https://nodeweekly.com/issues/458',
           })
-          .get('/issues/458')
-          .reply(200, '')
-      })
-
-      after(() => {
-        nock.restore()
+        nock('https://nodeweekly.com').head('/issues/458').reply(200, '')
       })
 
       it('gets the URL from the header', async () => {
         const html = load('./test/data/node-weekly-newsletter.html')
         const url = await new CooperPressHandler().findNewsletterUrl(html)
         expect(url).to.startWith('https://nodeweekly.com/issues/458')
-      }).timeout(10000)
+      })
     })
   })
 
