5f0d0ed69bb03e8766af79d57faf9d108195c1d7
Hongbo Wu <hongbo@omnivore.app>
2022-10-25 22:09:16 +0800

    Get tweet reply ids from thread id using puppeteer

--- packages/content-handler/src/websites/twitter-handler.ts
+++ packages/content-handler/src/websites/twitter-handler.ts
@@ -2,6 +2,7 @@ import { ContentHandler, PreHandleResult } from '../content-handler'
 import axios from 'axios'
 import { DateTime } from 'luxon'
 import _ from 'underscore'
+import puppeteer from 'puppeteer-core'
 
 interface TweetIncludes {
   users: {
@@ -134,85 +135,91 @@ const formatTimestamp = (timestamp: string) => {
   )
 }
 
+/**
+ * Wait for `ms` amount of milliseconds
+ * @param {number} ms
+ */
+const waitFor = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+/**
+ * Get tweets(even older than 7 days) using puppeteer
+ * @param {string} tweetId
+ */
+const getTweetIdsFromThreadId = async (tweetId: string): Promise<string[]> => {
+  const pageURL = `https://twitter.com/anyone/status/${tweetId}`
+
+  // Modify this variable to control the size of viewport
+  const factor = 0.2
+  const height = Math.floor(2000 / factor)
+  const width = Math.floor(1700 / factor)
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: {
+      width,
+      height,
+    },
+    args: [
+      `--force-device-scale-factor=${factor}`,
+      `--window-size=${width},${height}`,
+    ],
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto(pageURL, {
+    waitUntil: 'networkidle2',
+    timeout: 5 * 60 * 1000,
+  })
+
+  await waitFor(4000)
+
+  /** @type {string[]} */
+  const tweetIds = (await page.evaluate(async () => {
+    const ids: string[] = []
+
+    // Find the first Show thread button and click it
+    const showRepliesButton = Array.from(
+      document.querySelectorAll('div[dir="auto"]')
+    )
+      .filter((node) => node.children[0] && node.children[0].tagName === 'SPAN')
+      .find((node) => node.children[0].innerHTML === 'Show replies')
+
+    if (showRepliesButton) {
+      ;(showRepliesButton as HTMLElement).click()
+
+      await waitFor(2000)
+    }
+
+    const timeNodes = Array.from(document.querySelectorAll('time'))
+
+    for (const timeNode of timeNodes) {
+      /** @type {HTMLAnchorElement | HTMLSpanElement} */
+      const timeContainerAnchor = timeNode.parentElement
+      if (!timeContainerAnchor) continue
+
+      if (timeContainerAnchor?.tagName === 'SPAN') continue
+
+      const id = (timeContainerAnchor as HTMLAnchorElement).href
+        .split('/')
+        .reverse()[0]
+
+      ids.push(id)
+    }
+
+    return ids
+  })) as string[]
+
+  await browser.close()
+
+  return [tweetId, ...tweetIds]
+}
+
 export class TwitterHandler extends ContentHandler {
   constructor() {
     super()
     this.name = 'Twitter'
-  }
-
-  async getTweetIdsFromThreadId(tweetID: string): Promise<string[]> {
-    const pageURL = `https://twitter.com/anyone/status/${tweetID}`
-
-    // Modify this variable to control the size of viewport
-    const factor = 0.2
-    const height = Math.floor(2000 / factor)
-    const width = Math.floor(1700 / factor)
-
-    const browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: {
-        width,
-        height,
-      },
-      args: [
-        `--force-device-scale-factor=${factor}`,
-        `--window-size=${width},${height}`,
-      ],
-    })
-
-    const page = await browser.newPage()
-
-    await page.goto(pageURL, {
-      waitUntil: 'networkidle2',
-      timeout: 5 * 60 * 1000,
-    })
-
-    await waitFor(4000)
-
-    /** @type {string[]} */
-    const tweetIDs = await page.evaluate(async () => {
-      const ids = []
-
-      /**
-       * Wait for `ms` amount of milliseconds
-       * @param {number} ms
-       */
-      const waitFor = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-      // Find the first Show thread button and click it
-      const showRepliesButton = [
-        ...document.querySelectorAll('div[dir="auto"]'),
-      ]
-        .filter(
-          (node) => node.children[0] && node.children[0].tagName === 'SPAN'
-        )
-        .find((node) => node.children[0].innerHTML === 'Show replies')
-
-      if (showRepliesButton) {
-        showRepliesButton.click()
-
-        await waitFor(2000)
-      }
-
-      const timeNodes = Array.from(document.querySelectorAll('time'))
-
-      for (const timeNode of timeNodes) {
-        /** @type {HTMLAnchorElement | HTMLSpanElement} */
-        const timeContainerAnchor = timeNode.parentElement
-
-        if (timeContainerAnchor.tagName === 'SPAN') continue
-
-        const id = timeContainerAnchor.href.split('/').reverse()[0]
-
-        ids.push(id)
-      }
-
-      return ids
-    })
-
-    await browser.close()
-
-    return [tweetID, ...tweetIDs]
   }
 
   shouldPreHandle(url: string): boolean {
