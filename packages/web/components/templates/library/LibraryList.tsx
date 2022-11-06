4e1e68e9a3c7328ffc5a14409da7d7f66f6e3e87
Rupin Khandelwal <rupin@omnivore.app>
2022-11-06 19:30:18 -0300

    Drag and Drop feature

--- packages/web/components/templates/library/LibraryList.tsx
+++ packages/web/components/templates/library/LibraryList.tsx
@@ -1,14 +1,14 @@
+import { SetStateAction, useMemo, useState } from 'react'
+import Dropzone, { useDropzone } from 'react-dropzone'
 import { Box } from '../../elements/LayoutPrimitives'
 import { useGetViewerQuery } from '../../../lib/networking/queries/useGetViewerQuery'
 import { useGetUserPreferences } from '../../../lib/networking/queries/useGetUserPreferences'
-import { useMemo } from 'react'
 import { useGetLibraryItemsQuery } from '../../../lib/networking/queries/useGetLibraryItemsQuery'
 import { LinkedItemCardAction } from '../../patterns/LibraryCards/CardTypes'
 import { LibraryGridCard } from '../../patterns/LibraryCards/LibraryGridCard'
 import { LayoutCoordinator } from './LibraryContainer'
 import { EmptyLibrary } from '../homeFeed/EmptyLibrary'
 import Masonry from 'react-masonry-css'
-
 
 export type LibraryListProps = {
   layoutCoordinator: LayoutCoordinator
@@ -28,6 +28,11 @@ export function LibraryList(props: LibraryListProps): JSX.Element {
   const { itemsPages, size, setSize, isValidating, performActionOnItem } =
     useGetLibraryItemsQuery(defaultQuery)
 
+  const [fileNames, setFileNames] = useState([])
+
+  const handleDrop = (acceptedFiles) =>
+    setFileNames(acceptedFiles.map((file: { name: any }) => file.name))
+
   const libraryItems = useMemo(() => {
     const items =
       itemsPages?.flatMap((ad) => {
@@ -45,55 +50,80 @@ export function LibraryList(props: LibraryListProps): JSX.Element {
       />
     )
   }
+  console.log(fileNames)
 
   return (
     <Box css={{ overflowY: 'scroll' }}>
-      <Masonry
-        breakpointCols={props.layoutCoordinator.layout == 'LIST_LAYOUT' ? 1 : {
-          default: 3,
-          1200: 2,
-          992: 1
-        }}
-        className="omnivore-masonry-grid"
-        columnClassName="omnivore-masonry-grid_column"
-      >
-        {libraryItems.map((linkedItem) => (
-          <Box
-            className="linkedItemCard"
-            data-testid="linkedItemCard"
-            id={linkedItem.node.id}
-            tabIndex={0}
-            key={linkedItem.node.id}
-            css={{
-              width: '100%',
-              '&> div': {
-                bg: '$libraryBackground',
-              },
-              '&:focus': {
-                '> div': {
-                  bg: '$grayBgActive',
-                },
-              },
-              '&:hover': {
-                '> div': {
-                  bg: '$grayBgActive',
-                },
-              },
-            }}
-          >
-            {viewerData?.me && (
-              <LibraryGridCard
-                layout={props.layoutCoordinator.layout}
-                item={linkedItem.node}
-                viewer={viewerData.me}
-                handleAction={(action: LinkedItemCardAction) => {
-                  console.log('card clicked')
-                }}
-              />
-            )}
-          </Box>
-        ))}
-      </Masonry>
+      <Dropzone onDrop={handleDrop} preventDropOnDocument={true} noClick={true}>
+        {({ getRootProps, getInputProps, acceptedFiles, fileRejections }) => (
+          <div {...getRootProps({ className: 'dropzone' })}>
+            <p>Drag n drop files, or click to select files</p>
+            <input {...getInputProps()} />
+
+            <Masonry
+              breakpointCols={
+                props.layoutCoordinator.layout == 'LIST_LAYOUT'
+                  ? 1
+                  : {
+                      default: 3,
+                      1200: 2,
+                      992: 1,
+                    }
+              }
+              className="omnivore-masonry-grid"
+              columnClassName="omnivore-masonry-grid_column"
+            >
+              {libraryItems.map((linkedItem) => (
+                <Box
+                  className="linkedItemCard"
+                  data-testid="linkedItemCard"
+                  id={linkedItem.node.id}
+                  tabIndex={0}
+                  key={linkedItem.node.id}
+                  css={{
+                    width: '100%',
+                    '&> div': {
+                      bg: '$libraryBackground',
+                    },
+                    '&:focus': {
+                      '> div': {
+                        bg: '$grayBgActive',
+                      },
+                    },
+                    '&:hover': {
+                      '> div': {
+                        bg: '$grayBgActive',
+                      },
+                    },
+                  }}
+                >
+                  {viewerData?.me && (
+                    <LibraryGridCard
+                      layout={props.layoutCoordinator.layout}
+                      item={linkedItem.node}
+                      viewer={viewerData.me}
+                      handleAction={(action: LinkedItemCardAction) => {
+                        console.log('card clicked')
+                      }}
+                    />
+                  )}
+                </Box>
+              ))}
+            </Masonry>
+          </div>
+        )}
+      </Dropzone>
+
+      {/* Temporary code */}
+      <div>
+        <strong>Files:</strong>
+        <ul>
+          {fileNames.map((fileName) => (
+            <li key={fileName}>{fileName}</li>
+          ))}
+        </ul>
+      </div>
+
       {/* Extra padding at bottom to give space for scrolling */}
       <Box css={{ width: '100%', height: '200px' }} />
     </Box>
