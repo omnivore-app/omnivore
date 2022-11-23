c7d8769b66922c3a17119417119b4443a0732652
Jackson Harper <jacksonh@gmail.com>
2022-11-23 13:42:34 +0800

    Add UI for the rules engine

---
+++ packages/web/lib/networking/mutations/deleteRuleMutation.ts
@@ -0,0 +1,44 @@
+import { gql } from 'graphql-request'
+import { gqlFetcher } from '../networkHelpers'
+import { IntegrationType } from '../queries/useGetIntegrationsQuery'
+import { Rule } from '../queries/useGetRulesQuery'
+
+type DeleteRuleResult = {
+  deleteRule?: DeleteRuleData
+}
+
+type DeleteRuleData = {
+  deleteRule?: DeleteRuleSuccess
+  errorCodes?: unknown[]
+}
+
+type DeleteRuleSuccess = {
+  rule: Rule
+}
+
+export async function deleteRuleMutation(id: string): Promise<Integration> {
+  const mutation = gql`
+    mutation DeleteRule($id: ID!) {
+      deleteRule(id: $id) {
+        ... on DeleteRuleSuccess {
+          rule {
+            id
+            name
+          }
+        }
+        ... on DeleteRuleError {
+          errorCodes
+        }
+      }
+    }
+  `
+
+  const data = (await gqlFetcher(mutation, { id })) as DeleteRuleResult
+  const output = data as any
+  const error = data.deleteRule?.errorCodes?.find(() => true)
+  console.log('DATA: ', output.deleteRule)
+  if (error) {
+    throw error
+  }
+  return output.deleteRule?.rule
+}
