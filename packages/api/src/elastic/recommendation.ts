ef13cbf39a36f2d12f5821f45bffe3227c2f64a5
Hongbo Wu <hongbo@omnivore.app>
2022-12-08 12:33:05 +0800

    Recommend highlights

--- packages/api/src/elastic/recommendation.ts
+++ packages/api/src/elastic/recommendation.ts
@@ -4,28 +4,50 @@ import { createPage, getPageByParam, updatePage } from './pages'
 export const addRecommendation = async (
   ctx: PageContext,
   page: Page,
-  recommendation: Recommendation
+  recommendation: Recommendation,
+  highlightIds?: string[]
 ): Promise<string | undefined> => {
   try {
+    const highlights = page.highlights?.filter((highlight) =>
+      highlightIds?.includes(highlight.id)
+    )
+
     // check if the page is already recommended to the group
     const existingPage = await getPageByParam({
       userId: ctx.uid,
       url: page.url,
     })
     if (existingPage) {
-      if (existingPage.recommendations?.includes(recommendation)) {
+      const existingHighlights = existingPage.highlights || []
+
+      // remove duplicates
+      const newHighlights =
+        highlights?.filter(
+          (highlight) =>
+            !existingHighlights.find(
+              (existingHighlight) => existingHighlight.quote === highlight.quote
+            )
+        ) || []
+
+      const existingRecommendations = existingPage.recommendations || []
+      const isRecommended = existingRecommendations.some(
+        (existingRecommendation) =>
+          existingRecommendation.id === recommendation.id
+      )
+      if (isRecommended && newHighlights.length === 0) {
         return existingPage._id
       }
 
-      // update recommendedBy in the existing page
-      const recommendations = (existingPage.recommendations || []).concat(
-        recommendation
-      )
+      // update recommendations in the existing page
+      const recommendations = isRecommended
+        ? undefined
+        : existingRecommendations.concat(recommendation)
 
       await updatePage(
         existingPage.id,
         {
           recommendations,
+          highlights: existingHighlights.concat(newHighlights),
         },
         ctx
       )
@@ -41,7 +63,7 @@ export const addRecommendation = async (
       readingProgressPercent: 0,
       readingProgressAnchorIndex: 0,
       sharedAt: new Date(),
-      highlights: [],
+      highlights,
       readAt: undefined,
       labels: [],
     }
