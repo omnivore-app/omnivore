7ac26365b2c3099a623319815729dcf1f9042dfb
Jackson Harper <jacksonh@gmail.com>
2022-12-14 09:59:27 +0800

    Use let instead of var

--- packages/web/lib/highlights/highlightGenerator.ts
+++ packages/web/lib/highlights/highlightGenerator.ts
@@ -67,7 +67,7 @@ function nodeAttributesFromHighlight(
   const patch = highlight.patch
   const id = highlight.id
   const withNote = !!highlight.annotation
-  var customColor = undefined
+  let customColor = undefined
   const tooltip = undefined
   // We've disabled shared highlights, so passing undefined
   // here now, and removing the user object from highlights
