faf666d091d7d11688635a52a3cc4852ad3074c5
Satindar Dhillon <satindar@gmail.com>
2022-12-13 17:34:25 -0800

    add intercom to android app

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/OmnivoreApplication.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/OmnivoreApplication.kt
@@ -2,7 +2,17 @@ package app.omnivore.omnivore
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.intercom.android.sdk.Intercom
 
 @HiltAndroidApp
 class OmnivoreApplication: Application() {
+  override fun onCreate() {
+    super.onCreate()
+
+    Intercom.initialize(
+      this,
+      this.getString(R.string.intercom_api_key),
+      this.getString(R.string.intercom_app_id)
+    )
+  }
 }
