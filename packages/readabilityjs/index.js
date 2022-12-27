307e4c6091b7c46490de449c83e81bad2a1e5aa7
Hongbo Wu <hongbo@omnivore.app>
2022-12-27 17:15:35 +0800

    Add cloud function for readability

--- packages/readabilityjs/index.js
+++ packages/readabilityjs/index.js
@@ -1,7 +1,28 @@
-var Readability = require("./Readability");
-var isProbablyReaderable = require("./Readability-readerable");
+require('dotenv').config();
+const Sentry = require('@sentry/serverless');
+
+const Readability = require("./Readability");
+const isProbablyReaderable = require("./Readability-readerable");
+
+Sentry.GCPFunction.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 0,
+});
+
+async function parse(req, res) {
+  try {
+    const { document, options } = req.body;
+    const readability = new Readability(document, options);
+    const article = await readability.parse();
+
+    res.status(200).send({ article });
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+}
 
 module.exports = {
+  readability: Sentry.GCPFunction.wrapHttpFunction(parse),
   Readability: Readability,
   isProbablyReaderable: isProbablyReaderable
 };
