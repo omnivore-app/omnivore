abb5eade24a1b2ca52b8f6f42bd936a839fa39a5
Satindar Dhillon <satindar@gmail.com>
2023-01-03 10:37:35 -0800

    add properties to LinkedItem model to match ios model

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/persistence/entities/LinkedItem.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/persistence/entities/LinkedItem.kt
@@ -1,14 +1,18 @@
 package app.omnivore.omnivore.persistence.entities
 
 import androidx.core.net.toUri
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 data class LinkedItem(
-  val id: String,
+  @PrimaryKey val id: String,
   val title: String,
-  val createdAt: Any,
-  val savedAt: Any,
-  val readAt: Any?,
-  val updatedAt: Any?,
+  val createdAt: String,
+  val savedAt: String,
+  val readAt: String?,
+  val updatedAt: String?,
   val readingProgress: Double,
   val readingProgressAnchor: Int,
   val imageURLString: String?,
@@ -17,11 +21,27 @@ data class LinkedItem(
   val publisherURLString: String?,
   val siteName: String?,
   val author: String?,
-  val publishDate: Any?,
+  val publishDate: String?,
   val slug: String,
   val isArchived: Boolean,
-  val contentReader: String?,
-  val content: String?
+  val contentReader: String? = null,
+  val content: String? = null,
+  val createdId: String? = null,
+  val htmlContent: String? = null,
+  val language: String? = null,
+  val listenPositionIndex: Int? = null,
+  val listenPositionOffset: Double? = null,
+  val listenPositionTime: Double? = null,
+  val localPDF: String? = null,
+  val onDeviceImageURLString: String? = null,
+  val originalHtml: String? = null,
+  @ColumnInfo(typeAffinity = ColumnInfo.BLOB) val pdfData: ByteArray? = null,
+  val serverSyncStatus: Int = 0, // TODO: implement,
+  val tempPDFURL: String? = null
+
+// hasMany highlights
+// hasMany labels
+// has Many recommendations
 ) {
   fun publisherDisplayName(): String? {
     return publisherURLString?.toUri()?.host
@@ -30,5 +50,20 @@ data class LinkedItem(
   fun isPDF(): Boolean {
     val hasPDFSuffix = pageURLString.endsWith("pdf")
     return contentReader == "PDF" || hasPDFSuffix
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as LinkedItem
+
+    if (id != other.id) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    return id.hashCode()
   }
 }
