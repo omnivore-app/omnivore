dc444a471de36664b5a4ced5c22d0304e729c550
Jackson Harper <jacksonh@gmail.com>
2023-01-03 12:42:06 +0800

    Dont allow puppeteer to fail if empty tag is found

--- packages/puppeteer-parse/index.js
+++ packages/puppeteer-parse/index.js
@@ -590,7 +590,7 @@ async function retrieveHtml(page, logRecord) {
 
         try {
           // Removing blurred images since they are mostly the copies of lazy loaded ones
-          if (['img', 'image'].includes(el.tagName.toLowerCase())) {
+          if (el.tagName && ['img', 'image'].includes(el.tagName.toLowerCase())) {
             const filter = style.getPropertyValue('filter');
             if (filter && filter.startsWith('blur')) {
               el.parentNode && el.parentNode.removeChild(el);
