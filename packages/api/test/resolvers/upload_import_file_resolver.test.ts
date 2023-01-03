1b357f8c21399eef283243693688288f35baf2e9
Jackson Harper <jacksonh@gmail.com>
2023-01-03 11:07:21 +0800

    Add a test (have a feeling we will need to disable on CI due to GCS dependency)

---
+++ packages/api/test/resolvers/upload_import_file_resolver.test.ts
@@ -0,0 +1,63 @@
+import { createTestUser, deleteTestUser } from '../db'
+import { graphqlRequest, request } from '../util'
+import * as chai from 'chai'
+import { expect } from 'chai'
+import 'mocha'
+import { User } from '../../src/entity/user'
+import chaiString from 'chai-string'
+
+chai.use(chaiString)
+
+const uploadImportFile = async (
+  authToken: string,
+  fileType: string,
+  contentType: string
+) => {
+  const query = `
+  mutation {
+    uploadImportFile(type:${fileType}, contentType:"${contentType}") {
+      ... on UploadImportFileError {
+        errorCodes
+      }
+      ... on UploadImportFileSuccess {
+        uploadSignedUrl
+      }
+    }
+  }`
+
+  return graphqlRequest(query, authToken).expect(200)
+}
+
+describe('uploadImportFile API', () => {
+  let authToken: string
+  let user: User
+
+  before(async () => {
+    // create test user and login
+    user = await createTestUser('fakeUser')
+    const res = await request
+      .post('/local/debug/fake-user-login')
+      .send({ fakeEmail: user.email })
+
+    authToken = res.body.authToken
+  })
+
+  after(async () => {
+    await deleteTestUser(user.id)
+  })
+
+  describe('UploadImportFile', () => {
+    context('when it is a pocket file', () => {
+      it('should create an upload URL', async () => {
+        const res = await uploadImportFile(authToken, 'POCKET', 'text/csv')
+        expect(res.body.data.uploadImportFile.uploadSignedUrl).to.not.be.null
+      })
+    })
+    context('when it is a pocket file', () => {
+      it('should create an upload URL', async () => {
+        const res = await uploadImportFile(authToken, 'URL_LIST', 'text/csv')
+        expect(res.body.data.uploadImportFile.uploadSignedUrl).to.not.be.null
+      })
+    })
+  })
+})
