98497f26c064ad26b5829e5344a29f915464303e
Hongbo Wu <hongbo@omnivore.app>
2023-01-06 22:00:43 +0800

    Add demo user in docker-compose migrate service

--- packages/db/setup.sh
+++ packages/db/setup.sh
@@ -5,3 +5,9 @@ echo "created app_user"
 yarn workspace @omnivore/db migrate
 psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "GRANT omnivore_user TO app_user;"
 echo "granted omnivore_user to app_user"
+
+# create demo user with username: demo, password: demo
+USER_ID=$(uuidgen)
+PASSWORD='$2a$10$nxdWohvkWWpmQ28aTq0BK./pRgt6/WDrWyArvod8uWqxpFu/cfVoy'
+psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "INSERT INTO omnivore.user (id, source, email, source_user_id, name, password) VALUES ('$USER_ID', 'EMAIL', 'demo@omnivore.app', 'demo@omnivore.app', 'Demo User', '$PASSWORD'); INSERT INTO omnivore.user_profile (user_id, username) VALUES ('$USER_ID', 'demo');"
+echo "created demo user with username: demo, password: demo"
