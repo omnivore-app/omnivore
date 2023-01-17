7c10cc08c5e66aa13413f862e1bf68e5482fa7a5
Jackson Harper <jacksonh@gmail.com>
2023-01-17 21:02:28 +0800

    Mark emails as validating if no data returned

--- packages/web/lib/networking/queries/useGetNewsletterEmailsQuery.tsx
+++ packages/web/lib/networking/queries/useGetNewsletterEmailsQuery.tsx
@@ -64,7 +64,7 @@ export function useGetNewsletterEmailsQuery(): NewsletterEmailsQueryResponse {
     console.log('error', error)
   }
   return {
-    isValidating: false,
+    isValidating: true,
     emailAddresses: [],
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     revalidate: () => {},
