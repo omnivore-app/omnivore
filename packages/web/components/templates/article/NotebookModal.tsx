a3396997b2af33fb261cac7ee96b46c5cd69e575
Jackson Harper <jacksonh@gmail.com>
2023-02-01 17:38:43 +0800

    Handle PDF diffs when sorting the notebook

--- packages/web/components/templates/article/NotebookModal.tsx
+++ packages/web/components/templates/article/NotebookModal.tsx
@@ -66,11 +66,15 @@ export function NotebookModal(props: NotebookModalProps): JSX.Element {
       if (a.highlightPositionPercent && b.highlightPositionPercent) {
         return sorted(a.highlightPositionPercent, b.highlightPositionPercent)
       }
-      const aPos = getHighlightLocation(a.patch)
-      const bPos = getHighlightLocation(b.patch)
-      if (aPos && bPos) {
-        return sorted(aPos, bPos)
-      }
+      // We do this in a try/catch because it might be an invalid diff
+      // With PDF it will definitely be an invalid diff.
+      try {
+        const aPos = getHighlightLocation(a.patch)
+        const bPos = getHighlightLocation(b.patch)
+        if (aPos && bPos) {
+          return sorted(aPos, bPos)
+        }
+      } catch {}
       return a.createdAt.localeCompare(b.createdAt)
     })
   }, [props.highlights])
