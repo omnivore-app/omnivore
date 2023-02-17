1ee2a3e76d9b0789849d2e561cfe86eeff6dd8f0
Sixten Otto <opensource@sfko.com>
2023-02-17 12:35:17 -0700

    change analytics-swift package URL to HTTPS

--- apple/OmnivoreKit/Package.swift
+++ apple/OmnivoreKit/Package.swift
@@ -65,7 +65,7 @@ var dependencies: [Package.Dependency] {
     .package(url: "https://github.com/Square/Valet", from: "4.1.2"),
     .package(url: "https://github.com/maticzav/swift-graphql", from: "2.3.1"),
     .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.4"),
-    .package(url: "git@github.com:segmentio/analytics-swift.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/segmentio/analytics-swift.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "6.2.2")
   ]
   // Comment out following line for macOS build
