4d06e17ef311eb7e6c626d5f22d9d687edc37564
Jackson Harper <jacksonh@gmail.com>
2023-02-27 09:53:18 +0800

    Remove unused imports, linting

--- packages/web/components/templates/homeFeed/AddLinkModal.tsx
+++ packages/web/components/templates/homeFeed/AddLinkModal.tsx
@@ -5,14 +5,10 @@ import {
   ModalTitleBar,
   ModalButtonBar,
 } from '../../elements/ModalPrimitives'
-import { VStack, HStack, Box, SpanBox } from '../../elements/LayoutPrimitives'
+import { VStack, Box } from '../../elements/LayoutPrimitives'
 import { Button } from '../../elements/Button'
-import { StyledText } from '../../elements/StyledText'
-import { CrossIcon } from '../../elements/images/CrossIcon'
-import { theme } from '../../tokens/stitches.config'
 import { FormInput } from '../../elements/FormElements'
 import { useState, useCallback } from 'react'
-import { createArticleFromURLMutation } from '../../../lib/networking/mutations/createArticleFromURLMutation'
 import { saveUrlMutation } from '../../../lib/networking/mutations/saveUrlMutation'
 import toast from 'react-hot-toast'
 import { showErrorToast } from '../../../lib/toastHelpers'
@@ -24,42 +20,51 @@ type AddLinkModalProps = {
 export function AddLinkModal(props: AddLinkModalProps): JSX.Element {
   const [link, setLink] = useState('')
 
-  const handleLinkSubmission = useCallback(async (link: string) => {
-    const result = await saveUrlMutation(link)
-    // const result = await saveUrlMutation(link)
-    if (result && result.jobId) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      toast((t) => (
-        <Box>
-          Link Saved
-          <span style={{ padding: '16px' }} />
-          <Button
-            style="ctaDarkYellow"
-            autoFocus
-            onClick={() => {
-              window.location.href = `/article/sr/${result.jobId}`
-            }}
-          >
-            Read Now
-          </Button>
-        </Box>
-      ), { position: 'bottom-right' })
-    } else {
-      showErrorToast('Error saving link', { position: 'bottom-right' })
-    }
-  }, [link])
+  const handleLinkSubmission = useCallback(
+    async (link: string) => {
+      const result = await saveUrlMutation(link)
+      // const result = await saveUrlMutation(link)
+      if (result && result.jobId) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        toast(
+          (t) => (
+            <Box>
+              Link Saved
+              <span style={{ padding: '16px' }} />
+              <Button
+                style="ctaDarkYellow"
+                autoFocus
+                onClick={() => {
+                  window.location.href = `/article/sr/${result.jobId}`
+                }}
+              >
+                Read Now
+              </Button>
+            </Box>
+          ),
+          { position: 'bottom-right' }
+        )
+      } else {
+        showErrorToast('Error saving link', { position: 'bottom-right' })
+      }
+    },
+    [link]
+  )
 
-  const validateLink = useCallback((link: string) => {
-    try {
-      const url = new URL(link)
-      if (url.protocol !== 'https:' && url.protocol !== 'http:') {
+  const validateLink = useCallback(
+    (link: string) => {
+      try {
+        const url = new URL(link)
+        if (url.protocol !== 'https:' && url.protocol !== 'http:') {
+          return false
+        }
+      } catch (e) {
         return false
       }
-    } catch (e) {
-      return false
-    }
-    return true
-  }, [link])
+      return true
+    },
+    [link]
+  )
 
   return (
     <ModalRoot defaultOpen onOpenChange={props.onOpenChange}>
@@ -110,7 +115,10 @@ export function AddLinkModal(props: AddLinkModalProps): JSX.Element {
                   fontSize: '14px',
                 }}
               />
-              <ModalButtonBar onOpenChange={props.onOpenChange} acceptButtonLabel="Add Link" />
+              <ModalButtonBar
+                onOpenChange={props.onOpenChange}
+                acceptButtonLabel="Add Link"
+              />
             </form>
           </Box>
         </VStack>
