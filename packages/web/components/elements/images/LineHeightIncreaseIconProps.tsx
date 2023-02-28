1e38837f52cb093a64283fe302c6ec00065ee58f
Jackson Harper <jacksonh@gmail.com>
2023-02-28 18:22:28 +0800

    Add reader images

---
+++ packages/web/components/elements/images/LineHeightIncreaseIconProps.tsx
@@ -0,0 +1,60 @@
+type LineHeightIncreaseIconProps = {
+  strokeColor: string
+}
+
+export function LineHeightIncreaseIcon(
+  props: LineHeightIncreaseIconProps
+): JSX.Element {
+  return (
+    <svg
+      width="23"
+      height="14"
+      viewBox="0 0 23 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.2728 2.04089L21.8195 2.04089"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.2728 6.92468L21.8195 6.92468"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.2728 11.8081L21.8195 11.8081"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.20044 2.69069L3.27599 1.15112L1.35153 2.69069"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.20044 11.1583L3.27599 12.6979L1.35153 11.1583"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.276 1.15112L3.276 12.6978"
+        stroke="#969696"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
