b5a65cbc911baf2e8daa715164865b875f35c418
Satindar Dhillon <satindar@gmail.com>
2023-03-07 20:41:15 -0800

    add create label gql mutation

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/networking/SavedItemLabelMutations.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/networking/SavedItemLabelMutations.kt
@@ -1,6 +1,8 @@
 package app.omnivore.omnivore.networking
 
+import app.omnivore.omnivore.graphql.generated.CreateLabelMutation
 import app.omnivore.omnivore.graphql.generated.SetLabelsMutation
+import app.omnivore.omnivore.graphql.generated.type.CreateLabelInput
 import app.omnivore.omnivore.graphql.generated.type.SetLabelsInput
 
 suspend fun Networker.updateLabelsForSavedItem(input: SetLabelsInput): Boolean {
@@ -9,5 +11,14 @@ suspend fun Networker.updateLabelsForSavedItem(input: SetLabelsInput): Boolean {
     return result.data?.setLabels?.onSetLabelsSuccess?.labels != null
   } catch (e: java.lang.Exception) {
     false
+  }
+}
+
+suspend fun Networker.createNewLabel(input: CreateLabelInput): CreateLabelMutation.Label? {
+  return try {
+    val result = authenticatedApolloClient().mutation(CreateLabelMutation(input)).execute()
+    return result.data?.createLabel?.onCreateLabelSuccess?.label
+  } catch (e: java.lang.Exception) {
+    null
   }
 }
