dd7a4e4eac6966a156336a6ece394d5c1fc0f70f
Hongbo Wu <hongbo@omnivore.app>
2023-03-09 16:00:17 +0800

    Use local timezone for the date in the reader

--- packages/web/lib/dateFormatting.ts
+++ packages/web/lib/dateFormatting.ts
@@ -1,15 +1,19 @@
 //https://github.com/you-dont-need/You-Dont-Need-Momentjs
 
 const locale = Intl.DateTimeFormat().resolvedOptions().locale || 'en-US'
+// get the user's time zone
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
 export function formattedLongDate(rawDate: string): string {
   return new Intl.DateTimeFormat(locale, {
     dateStyle: 'long',
+    timeZone,
   }).format(new Date(rawDate))
 }
 
 export function formattedShortDate(rawDate: string): string {
   return new Intl.DateTimeFormat(locale, {
     dateStyle: 'short',
+    timeZone,
   }).format(new Date(rawDate))
 }
