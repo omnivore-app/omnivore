7102107890d48cd5963883f7a34438dd2210b794
Jackson Harper <jacksonh@gmail.com>
2023-03-17 16:16:01 +0800

    Improve the UI for the browser extension

---
+++ pkg/extension/src/scripts/api.js
@@ -0,0 +1,88 @@
+function updateLabelsCache(apiUrl, tab) {
+  const query = JSON.stringify({
+    query: `query GetLabels {
+      labels {
+        ... on LabelsSuccess {
+          labels {
+            ...LabelFields
+          }
+        }
+        ... on LabelsError {
+          errorCodes
+        }
+      }
+    }
+    fragment LabelFields on Label {
+      id
+      name
+      color
+      description
+      createdAt
+    }
+    `,
+  })
+  return fetch(apiUrl, {
+    method: 'POST',
+    redirect: 'follow',
+    credentials: 'include',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: query,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const result = data.data.labels.labels
+      return result
+    })
+    .then((labels) => {
+      setStorage({
+        labels: labels,
+        labelsLastUpdated: new Date().toISOString(),
+      })
+      return labels
+    })
+}
+
+function updatePageTitle(apiUrl, pageId, title) {
+  console.log('updated title: ', apiUrl, pageId, title)
+  const mutation = JSON.stringify({
+    query: `mutation UpdatePage($input: UpdatePageInput!) {
+      updatePage(input: $input) {
+        ... on UpdatePageSuccess {
+          updatedPage {
+            id
+          }
+        }
+        ... on UpdatePageError {
+          errorCodes
+        }
+      }
+    }
+  `,
+    variables: {
+      input: {
+        pageId,
+        title,
+      },
+    },
+  })
+
+  return fetch(apiUrl, {
+    method: 'POST',
+    redirect: 'follow',
+    credentials: 'include',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: mutation,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('updated title: ', data)
+    })
+}
