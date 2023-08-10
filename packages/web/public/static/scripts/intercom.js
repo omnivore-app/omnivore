89631cc266a79ddedadd729149a947243330c0ea
Jackson Harper <jacksonh@gmail.com>
2023-08-10 11:14:17 +0800

    Remove debug from intercom script

--- packages/web/public/static/scripts/intercom.js
+++ packages/web/public/static/scripts/intercom.js
@@ -1,13 +1,11 @@
 ;(function () {
   var w = window
   var ic = w.Intercom
-  console.log('running the intercom script, checking if we are injected: ', ic)
 
   if (typeof ic === 'function') {
     ic('reattach_activator')
     ic('update', w.intercomSettings)
   } else {
-    console.log('injecting the intercom widget')
     var d = document
     var i = function () {
       i.c(arguments)
@@ -18,8 +16,6 @@
     }
     w.Intercom = i
     var l = function () {
-      console.log(' running intercom onload script ')
-
       var s = d.createElement('script')
       s.type = 'text/javascript'
       s.async = true
