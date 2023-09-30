10e768ace0b53a6b5720e024323f8dc2b2eddf1b
Jackson Harper <jacksonh@gmail.com>
2023-09-30 12:54:10 +0800

    Remove debug

--- packages/web/components/patterns/LibraryCards/LibraryGridCard.tsx
+++ packages/web/components/patterns/LibraryCards/LibraryGridCard.tsx
@@ -183,12 +183,6 @@ const LibraryGridCardContent = (props: LinkedItemCardProps): JSX.Element => {
   const { isChecked, setIsChecked, item } = props
   const [menuOpen, setMenuOpen] = useState(false)
   const originText = siteName(props.item.originalArticleUrl, props.item.url)
-  console.log(
-    'const originText = siteName(props.item.originalArticleUrl, props.item.url)',
-    siteName(props.item.originalArticleUrl, props.item.url),
-    props.item.originalArticleUrl,
-    props.item.url
-  )
 
   const handleCheckChanged = useCallback(() => {
     const newValue = !isChecked
