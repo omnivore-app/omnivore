4948801d7bc5c4a4e06e596bdfc20194f8a72ecb
Hongbo Wu <hongbo@omnivore.app>
2023-10-03 18:52:11 +0800

    fix a typo

--- packages/db/migrations/0128.do.create_index_for_foreign_key.sql
+++ packages/db/migrations/0128.do.create_index_for_foreign_key.sql
@@ -4,7 +4,7 @@
 
 BEGIN;
 
-CREATE INDEX IF NOT EXISTS entity_labels_library_item_id_idx ON omnivore.entity_labels(library_item_id)
+CREATE INDEX IF NOT EXISTS entity_labels_library_item_id_idx ON omnivore.entity_labels(library_item_id);
 CREATE INDEX IF NOT EXISTS entity_labels_highlight_id_idx ON omnivore.entity_labels(highlight_id);
 
 CREATE INDEX IF NOT EXISTS highlight_library_item_id_idx ON omnivore.highlight (library_item_id);
