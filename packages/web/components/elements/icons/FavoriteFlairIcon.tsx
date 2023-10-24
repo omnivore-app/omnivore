65b20fcb866f41c4c360b168a7d3d684595fe093
Jackson Harper <jacksonh@gmail.com>
2023-10-24 18:13:32 +0800

    Revert unneeded import

--- packages/web/components/elements/icons/FavoriteFlairIcon.tsx
+++ packages/web/components/elements/icons/FavoriteFlairIcon.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable functional/no-class */
 /* eslint-disable functional/no-this-expression */
-import { SpanBox } from '../LayoutPrimitives'
 import { IconProps } from './IconProps'
 
 import React from 'react'
