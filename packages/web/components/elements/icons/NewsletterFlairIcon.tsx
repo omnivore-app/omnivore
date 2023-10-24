a95a5ac2a5c8452d6e806e94956ab2b203249a84
Jackson Harper <jacksonh@gmail.com>
2023-10-24 17:08:24 +0800

    Add flair icon

---
+++ packages/web/components/elements/icons/NewsletterFlairIcon.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable functional/no-class */
+/* eslint-disable functional/no-this-expression */
+import { IconProps } from './IconProps'
+
+import React from 'react'
+
+export class NewsletterFlairIcon extends React.Component<IconProps> {
+  render() {
+    const size = (this.props.size || 26).toString()
+    const color = (this.props.color || '#2A2A2A').toString()
+
+    return (
+      <svg
+        width="17"
+        height="17"
+        viewBox="0 0 17 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g>
+          <path
+            d="M15.4056 5.58984V11.8998C15.4056 12.41 15.2107 12.9009 14.8607 13.272C14.5108 13.6432 14.0322 13.8666 13.5229 13.8965L13.4056 13.8998H4.07227C3.56213 13.8999 3.07126 13.705 2.70009 13.355C2.32893 13.005 2.10553 12.5264 2.0756 12.0172L2.07227 11.8998V5.58984L8.36893 9.78784L8.44627 9.83184C8.5374 9.87637 8.6375 9.89952 8.73893 9.89952C8.84037 9.89952 8.94046 9.87637 9.0316 9.83184L9.10893 9.78784L15.4056 5.58984Z"
+            fill="#007AFF"
+          />
+          <path
+            d="M13.4053 3.23242C14.1253 3.23242 14.7567 3.61242 15.1087 4.18376L8.73865 8.43042L2.36865 4.18376C2.53581 3.91227 2.76546 3.68469 3.03846 3.52001C3.31145 3.35533 3.61987 3.25833 3.93799 3.23709L4.07199 3.23242H13.4053Z"
+            fill="#007AFF"
+          />
+        </g>
+      </svg>
+    )
+  }
+}
