18de68f90364076a5a453a09ddb7e35fd607178a
Jackson Harper <jacksonh@gmail.com>
2023-10-24 15:35:41 +0800

    Create a database for firefish

--- packages/db/setup.sh
+++ packages/db/setup.sh
@@ -11,3 +11,6 @@ USER_ID=$(uuidgen)
 PASSWORD='$2a$10$41G6b1BDUdxNjH1QFPJYDOM29EE0C9nTdjD1FoseuQ8vZU1NWtrh6'
 psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "INSERT INTO omnivore.user (id, source, email, source_user_id, name, password) VALUES ('$USER_ID', 'EMAIL', 'demo@omnivore.app', 'demo@omnivore.app', 'Demo User', '$PASSWORD'); INSERT INTO omnivore.user_profile (user_id, username) VALUES ('$USER_ID', 'demo_user');"
 echo "created demo user with email: demo@omnivore.app, password: demo_password"
+
+psql --host $PG_HOST -U $PG_USER -d $PG_DB -c "CREATE DATABASE firefish"  || true
+
