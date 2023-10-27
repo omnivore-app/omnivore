f8a6fc9e0d6322d7d4487a2ece17c740054f7a8a
Lily Mara <lilymara@fastmail.com>
2023-10-27 08:44:26 -0700

    Improve developer environment onboarding
    I encountered a number of issues while trying to get my own local dev
    environment set up. This change adds missing information to the readme, updates
    the Postgres image to one that includes required module dependencies, and
    removes next.js rewrite rules (IS THIS SAFE?) which were no longer used and
    causing issues.

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -24,28 +24,6 @@ const moduleExports = {
       'proxy.omnivore-image-cache.app',
     ],
   },
-  rewrites: () => [
-    {
-      source: '/api/graphql',
-      destination: `https://api-${process.env.NEXT_PUBLIC_APP_ENV}.omnivore.app/api/graphql`,
-    },
-    {
-      source: '/api/auth/:path*',
-      destination: `https://api-${process.env.NEXT_PUBLIC_APP_ENV}.omnivore.app/api/auth/:path*`,
-    },
-    {
-      source: '/api/article/save',
-      destination: `https://api-${process.env.NEXT_PUBLIC_APP_ENV}.omnivore.app/api/article/save`,
-    },
-    {
-      source: '/api/mobile-auth/:path*',
-      destination: `https://api-${process.env.NEXT_PUBLIC_APP_ENV}.omnivore.app/api/mobile-auth/:path*`,
-    },
-    {
-      source: '/collect/:match*',
-      destination: 'https://app.posthog.com/:match*',
-    },
-  ],
   async headers() {
     return [
       {
