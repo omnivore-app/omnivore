a52869212568f216c7b8c4cc489adb43b717d96b
Hongbo Wu <hongbo@omnivore.app>
2023-10-30 15:12:18 +0800

    add import_item_state to integration entity

--- packages/api/src/entity/integration.ts
+++ packages/api/src/entity/integration.ts
@@ -14,6 +14,13 @@ export enum IntegrationType {
   Import = 'IMPORT',
 }
 
+export enum ImportItemState {
+  UNREAD = 'UNREAD',
+  UNARCHIVED = 'UNARCHIVED',
+  ARCHIVED = 'ARCHIVED',
+  ALL = 'ALL',
+}
+
 @Entity({ name: 'integrations' })
 export class Integration {
   @PrimaryGeneratedColumn('uuid')
@@ -49,4 +56,7 @@ export class Integration {
 
   @Column('text', { nullable: true })
   taskName?: string | null
+
+  @Column('enum', { enum: ImportItemState, nullable: true })
+  importItemState?: ImportItemState | null
 }
