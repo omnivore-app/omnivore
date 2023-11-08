1e336dd61cc63500a26054bfff2fd4bb851f74d5
Jackson Harper <jacksonh@gmail.com>
2023-11-08 13:44:08 +0800

    Use CLIENT_URL instead of hard coded values

--- packages/api/src/env.ts
+++ packages/api/src/env.ts
@@ -3,16 +3,5 @@ import { getEnv } from './util'
 export const env = getEnv()
 
 export function homePageURL(): string {
-  switch (env.server.apiEnv) {
-    case 'local':
-      return 'http://localhost:3000'
-    case 'demo':
-      return 'https://demo.omnivore.app'
-    case 'qa':
-      return 'https://web-dev.omnivore.app'
-    case 'prod':
-      return 'https://omnivore.app'
-  }
-
-  return 'https://omnivore.app'
+  return env.client.url
 }
