f4ff4b9fcda8891996798e5425390bebedb5ce43
Hongbo Wu <hongbo@omnivore.app>
2024-01-04 22:27:31 +0800

    fix tests

--- packages/api/test/global-setup.ts
+++ packages/api/test/global-setup.ts
@@ -1,9 +1,16 @@
+import { env } from '../src/env'
+import { redisClient } from '../src/redis'
 import { createTestConnection } from './db'
 import { startApolloServer } from './util'
 
 export const mochaGlobalSetup = async () => {
   await createTestConnection()
   console.log('db connection created')
+
+  if (env.redis.url) {
+    await redisClient.connect()
+    console.log('redis connection created')
+  }
 
   await startApolloServer()
   console.log('apollo server started')
