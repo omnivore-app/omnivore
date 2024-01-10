525b3fbacfbe13df5e54e3d3265736c1720f5d34
Hongbo Wu <hongbo@omnivore.app>
2024-01-10 17:32:23 +0800

    add index for sorting by read_at

--- packages/db/migrations/0155.do.library_item_user_id_published_at_idx.sql
+++ packages/db/migrations/0155.do.library_item_user_id_published_at_idx.sql
@@ -3,4 +3,4 @@
 -- Description: Add library_item_user_id_published_at_idx index on library_item table for user_id and published_at
 
 -- create index for sorting concurrently to avoid locking
-CREATE INDEX CONCURRENTLY IF NOT EXISTS library_item_user_id_updated_at_idx ON omnivore.library_item (user_id, updated_at DESC NULLS LAST);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS library_item_user_id_published_at_idx ON omnivore.library_item (user_id, published_at DESC NULLS LAST);
