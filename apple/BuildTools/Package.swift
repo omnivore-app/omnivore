17bae460b4b4982547862a6f87048c68f834b778
Satindar Dhillon <satindar@gmail.com>
2024-01-20 08:18:42 -0800

    use swiftformat plugin to run formatter

--- apple/BuildTools/Package.swift
+++ apple/BuildTools/Package.swift
@@ -5,7 +5,6 @@ let package = Package(
   name: "BuildTools",
   platforms: [.macOS(.v10_11)],
   dependencies: [
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.11"),
     .package(url: "https://github.com/realm/SwiftLint", from: "0.44.0"),
     .package(url: "https://github.com/maticzav/swift-graphql", from: "2.2.1")
   ],
