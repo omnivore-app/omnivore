e407db914a3983f20365b40d1e4861a9bc67df89
Jackson Harper <jacksonh@gmail.com>
2024-01-22 15:35:14 +0800

    Add new job for handling upload content

--- packages/api/src/routers/svc/content.ts
+++ packages/api/src/routers/svc/content.ts
@@ -2,25 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import express from 'express'
-import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity'
-import { LibraryItem, LibraryItemState } from '../../entity/library_item'
 import { readPushSubscription } from '../../pubsub'
-import { authTrx } from '../../repository'
-import { libraryItemRepository } from '../../repository/library_item'
-import { updateLibraryItem } from '../../services/library_item'
-import {
-  findUploadFileById,
-  setFileUploadComplete,
-} from '../../services/upload_file'
 import { logger } from '../../utils/logger'
-
-interface UpdateContentMessage {
-  fileId: string
-  content: string
-  title?: string
-  author?: string
-  description?: string
-}
+import {
+  UpdateContentMessage,
+  updateContentForFileItem,
+} from '../../services/update_pdf_content'
 
 export function contentServiceRouter() {
   const router = express.Router()
@@ -48,75 +35,12 @@ export function contentServiceRouter() {
       return
     }
     const msg = data as UpdateContentMessage
-
-    // First attempt to parse the file id out of the name
-    const parts = msg.fileId.split('/')
-    const fileId = parts && parts.length > 1 ? parts[1] : undefined
-    if (!fileId) {
-      logger.info('No file id found in message')
-      res.status(200).send('Bad Request')
-      return
-    }
-
-    const uploadFile = await findUploadFileById(fileId)
-    if (!uploadFile) {
-      logger.info('No file found')
-      res.status(404).send('No file found')
-      return
-    }
-
-    const libraryItem = await authTrx(
-      async (tx) =>
-        tx
-          .withRepository(libraryItemRepository)
-          .createQueryBuilder('item')
-          .innerJoinAndSelect('item.uploadFile', 'file')
-          .where('item.user = :userId', { userId: uploadFile.user.id })
-          .andWhere('file.id = :fileId', { fileId })
-          .getOne(),
-      undefined,
-      uploadFile.user.id
-    )
-    if (!libraryItem) {
-      logger.info(`No upload file found for id: ${fileId}`)
+    if (!updateContentForFileItem(msg)) {
       res.status(404).send('Bad Request')
       return
     }
-
-    const itemToUpdate: QueryDeepPartialEntity<LibraryItem> = {
-      originalContent: msg.content,
-    }
-    if (msg.title) itemToUpdate.title = msg.title
-    if (msg.author) itemToUpdate.author = msg.author
-    if (msg.description) itemToUpdate.description = msg.description
-
-    // This event is fired after the file is fully uploaded,
-    // so along with updating content, we mark it as
-    // succeeded.
-    itemToUpdate.state = LibraryItemState.Succeeded
-
-    try {
-      const uploadFileData = await setFileUploadComplete(
-        fileId,
-        uploadFile.user.id
-      )
-      logger.info('updated uploadFileData', uploadFileData)
-    } catch (error) {
-      logger.info('error marking file upload as completed', error)
-    }
-
-    const result = await updateLibraryItem(
-      libraryItem.id,
-      itemToUpdate,
-      uploadFile.user.id
-    )
-    logger.info('Updating library item text', {
-      id: libraryItem.id,
-      result,
-      content: msg.content.substring(0, 20),
-    })
-
-    res.status(200).send(msg)
+    res.sendStatus(200)
+    return
   })
 
   return router
