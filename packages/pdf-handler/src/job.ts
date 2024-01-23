c7cfb8693f95ec7875b9d64eb776e5458d722afa
Jackson Harper <jacksonh@gmail.com>
2024-01-23 23:46:59 +0800

    Remove unused change

--- packages/pdf-handler/src/job.ts
+++ packages/pdf-handler/src/job.ts
@@ -4,13 +4,6 @@ import { redisDataSource } from './redis_data_source'
 const QUEUE_NAME = 'omnivore-backend-queue'
 const JOB_NAME = 'update-pdf-content'
 
-interface savePageJob {
-  userId: string
-  data: unknown
-  isRss: boolean
-  isImport: boolean
-}
-
 const queue = new Queue(QUEUE_NAME, {
   connection: redisDataSource.queueRedisClient,
 })
