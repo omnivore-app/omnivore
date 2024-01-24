9e966bb2696569999219e3e0ffe07e8baa9fc41d
Hongbo Wu <hongbo@omnivore.app>
2024-01-24 11:26:35 +0800

    add REFRESH_ERROR to the status of subscriptions and add a new column refreshed_at

---
+++ packages/db/migrations/0159.do.alter_subscriptions.sql
@@ -0,0 +1,19 @@
+-- Type: DO
+-- Name: alter_subscriptions
+-- Description: Alter omnivore.subscriptions table to add a new state and date column
+
+BEGIN;
+
+ALTER TYPE subscription_status_type
+    ADD VALUE IF NOT EXISTS 'REFRESH_ERROR';
+
+ALTER TABLE omnivore.subscriptions
+    ADD COLUMN refreshed_at timestamptz;
+UPDATE omnivore.subscriptions
+    SET refreshed_at = last_fetched_at
+    WHERE last_fetched_at IS NOT NULL;
+
+ALTER TABLE omnivore.subscriptions
+    RENAME COLUMN last_fetched_at TO most_recent_item_date;
+
+COMMIT;
