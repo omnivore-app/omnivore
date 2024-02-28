3af62d5d89acd545935b0b082d6f73c6cfbf88ed
Hongbo Wu <hongbo@omnivore.app>
2024-02-28 17:13:11 +0800

    fix typo

--- packages/api/src/services/library_item.ts
+++ packages/api/src/services/library_item.ts
@@ -279,7 +279,7 @@ export const buildQueryString = (
               // return only deleted pages within 14 days
               return "(library_item.state = 'DELETED' AND library_item.deleted_at >= now() - interval '14 days')"
             default: {
-              let sql = "library_item.state = 'ARCHIVED'"
+              let sql = "library_item.state <> 'ARCHIVED'"
               if (useFolders) {
                 const param = `folder_${parameters.length}`
                 const folderSql = escapeQueryWithParameters(
