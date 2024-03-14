71b9e5e4d5fce4d6711a6b6e2d922894abf3ce0a
Jackson Harper <jacksonh@gmail.com>
2024-03-14 17:55:04 +0800

    More linting

--- packages/discord/src/index.ts
+++ packages/discord/src/index.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import {
   Client,
   Partials,
