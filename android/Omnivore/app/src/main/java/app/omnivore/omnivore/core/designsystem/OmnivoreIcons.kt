23975fdde188311bc6fa56cb3b3953acf6eeaf00
Stefano Sansone <stefanosansone.dev@gmail.com>
2024-04-06 23:01:51 +0200

    add icons and icons object

---
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/core/designsystem/OmnivoreIcons.kt
@@ -0,0 +1,15 @@
+package app.omnivore.omnivore.core.designsystem
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bookmarks
+import androidx.compose.material.icons.rounded.Person
+import app.omnivore.omnivore.R
+
+object OmnivoreIcons {
+    val Following = R.drawable.ic_stacks_rounded_fill
+    val FollowingEmpty = R.drawable.ic_stacks_rounded_empty
+    val Inbox = Icons.Rounded.Bookmarks
+    val InboxEmpty = R.drawable.ic_bookmarks_rounded_empty
+    val Profile = Icons.Rounded.Person
+    val ProfileEmpty = R.drawable.ic_person_rounded_empty
+}
