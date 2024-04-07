4cf6bf656b3179bbf9ef5a30dba602c9b494184f
Stefano Sansone <stefanosansone.dev@gmail.com>
2024-04-07 12:59:35 +0200

    add every icon as drawable

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/core/designsystem/OmnivoreIcons.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/core/designsystem/OmnivoreIcons.kt
@@ -1,15 +1,12 @@
 package app.omnivore.omnivore.core.designsystem
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Bookmarks
-import androidx.compose.material.icons.rounded.Person
 import app.omnivore.omnivore.R
 
 object OmnivoreIcons {
     val Following = R.drawable.ic_stacks_rounded_fill
     val FollowingEmpty = R.drawable.ic_stacks_rounded_empty
-    val Inbox = Icons.Rounded.Bookmarks
+    val Inbox = R.drawable.ic_bookmarks_rounded_fill
     val InboxEmpty = R.drawable.ic_bookmarks_rounded_empty
-    val Profile = Icons.Rounded.Person
+    val Profile = R.drawable.ic_person_rounded_fill
     val ProfileEmpty = R.drawable.ic_person_rounded_empty
 }
