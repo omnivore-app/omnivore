33e8e4b75a98efb779e254a781c257c83c06a908
Jackson Harper <jacksonh@gmail.com>
2024-04-08 16:02:03 +0800

    Bump database version

--- android/Omnivore/app/src/main/java/app/omnivore/omnivore/core/database/OmnivoreDatabase.kt
+++ android/Omnivore/app/src/main/java/app/omnivore/omnivore/core/database/OmnivoreDatabase.kt
@@ -27,7 +27,7 @@ import app.omnivore.omnivore.core.database.entities.ViewerDao
         HighlightChange::class,
         SavedItemAndSavedItemLabelCrossRef::class,
         SavedItemAndHighlightCrossRef::class],
-    version = 24,
+    version = 26,
     exportSchema = true
 )
 abstract class OmnivoreDatabase : RoomDatabase() {
