6ce95194ee20443c8d716b9d28a2f3e33a2718cb
Hongbo Wu <hongbo@omnivore.app>
2024-04-16 11:41:19 +0800

    alter read_labels policy to check user_id

---
+++ packages/db/migrations/0173.do.alter_labels_table_policy.sql
@@ -0,0 +1,11 @@
+-- Type: DO
+-- Name: alter_labels_table_policy
+-- Description: Alter labels table select policy to check user_id
+
+BEGIN;
+
+ALTER POLICY read_labels ON omnivore.labels
+    TO omnivore_user
+    USING (user_id = omnivore.get_current_user_id());
+
+COMMIT;
