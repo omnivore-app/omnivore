543ecd0d9108407cf68f510a6043bb2ff59f43b4
Jackson Harper <jacksonh@gmail.com>
2024-05-08 19:17:47 +0800

    Fix colours on ConfirmProfile and update naming

--- packages/web/pages/confirm-profile.tsx
+++ packages/web/pages/confirm-profile.tsx
@@ -1,13 +1,13 @@
 import { PageMetaData } from '../components/patterns/PageMetaData'
 import { ProfileLayout } from '../components/templates/ProfileLayout'
-import { ConfirmProfileModal } from '../components/templates/ConfirmProfileModal'
+import { ConfirmProfileForm } from '../components/templates/ConfirmProfileForm'
 
 export default function ConfirmProfilePage(): JSX.Element {
   return (
     <>
       <PageMetaData title="Create Profile - Omnivore" path="/confirm-profile" />
       <ProfileLayout>
-        <ConfirmProfileModal />
+        <ConfirmProfileForm />
       </ProfileLayout>
       <div data-testid="confirm-profile-page-tag" />
     </>
