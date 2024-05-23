912fc566a96db18c9e1f198dcb521e1a45fc028a
Hongbo Wu <hongbo@omnivore.app>
2024-05-23 11:32:14 +0800

    add approved column and seen_at

--- packages/db/migrations/0177.do.public_item.sql
+++ packages/db/migrations/0177.do.public_item.sql
@@ -8,9 +8,10 @@ CREATE TABLE omnivore.public_item_source (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
     name TEXT NOT NULL,
     topics TEXT[] NOT NULL,
-    thumbnail TEXT NOT NULL,
+    thumbnail TEXT,
     url TEXT NOT NULL,
     language_codes TEXT[] NOT NULL,
+    approved BOOLEAN NOT NULL DEFAULT FALSE,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -21,6 +22,7 @@ CREATE TABLE omnivore.public_item (
     type TEXT NOT NULL, -- public feeds, newsletters, or user recommended
     title TEXT NOT NULL,
     url TEXT NOT NULL,
+    approved BOOLEAN NOT NULL DEFAULT FALSE,
     thumbnail TEXT,
     preview_content TEXT,
     language_code TEXT,
@@ -67,5 +69,16 @@ CREATE TABLE omnivore.public_item_interactions (
 
 CREATE INDEX public_item_interaction_user_id_idx ON omnivore.public_item_interactions(user_id);
 CREATE INDEX public_item_interaction_public_item_id_idx ON omnivore.public_item_interactions(public_item_id);
+
+CREATE TABLE omnivore.library_item_interactions (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+    user_id uuid NOT NULL REFERENCES omnivore.user(id) ON DELETE CASCADE,
+    library_item_id uuid NOT NULL REFERENCES omnivore.library_item(id) ON DELETE CASCADE,
+    action TEXT NOT NULL, -- seen
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX library_item_interaction_user_id_idx ON omnivore.library_item_interactions(user_id);
+CREATE INDEX library_item_interaction_library_item_id_idx ON omnivore.library_item_interactions(library_item_id);
 
 COMMIT;
