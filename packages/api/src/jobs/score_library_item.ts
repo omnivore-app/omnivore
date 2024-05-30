3c27125645d8cf3f543d790b0db07a8d1448487d
Hongbo Wu <hongbo@omnivore.app>
2024-05-30 14:34:08 +0800

    trigger update home job once a new item is scored

--- packages/api/src/jobs/score_library_item.ts
+++ packages/api/src/jobs/score_library_item.ts
@@ -3,6 +3,7 @@ import {
   updateLibraryItem,
 } from '../services/library_item'
 import { Feature, getScores } from '../services/score'
+import { enqueueUpdateHomeJob } from '../utils/createTask'
 import { lanaugeToCode } from '../utils/helpers'
 import { logger } from '../utils/logger'
 
@@ -81,4 +82,9 @@ export const scoreLibraryItem = async (
     true
   )
   logger.info('Library item scored', data)
+
+  await enqueueUpdateHomeJob({
+    userId,
+    cursor: Date.now(),
+  })
 }
