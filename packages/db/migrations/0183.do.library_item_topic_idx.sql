6a54d651a8fff6f81a11c9e904b24caca1723175
Hongbo Wu <hongbo@omnivore.app>
2024-06-19 16:13:05 +0800

    do not create library_item_topic_idx index on library_item table since it locks the table/row for a long time

--- packages/db/migrations/0183.do.library_item_topic_idx.sql
+++
@@ -1,5 +0,0 @@
--- Type: DO
--- Name: library_item_topic_idx
--- Description: Create index on topic column in library_item table
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS library_item_topic_idx ON omnivore.library_item USING GIST (topic);
--- packages/db/migrations/0183.undo.library_item_topic_idx.sql
+++
@@ -1,9 +0,0 @@
--- Type: UNDO
--- Name: library_item_topic_idx
--- Description: Create index on topic column in library_item table
-
-BEGIN;
-
-DROP INDEX IF EXISTS omnivore.library_item_topic_idx;
-
-COMMIT;
