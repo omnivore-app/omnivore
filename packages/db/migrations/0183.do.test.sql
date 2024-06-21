538eb382175ce024e0432aa587cbaf7b4e11432a
Hongbo Wu <hongbo@omnivore.app>
2024-06-21 18:06:12 +0800

    more testing

--- packages/db/migrations/0183.do.test.sql
+++ packages/db/migrations/0183.do.test.sql
@@ -11,6 +11,4 @@ CREATE TABLE omnivore.test (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TRIGGER update_test_modtime BEFORE UPDATE ON omnivore.test FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
-
 COMMIT;
