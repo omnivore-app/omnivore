de3861e63fc6a512ff71f20459fe2b79f154bd49
Stefano Sansone <stefanosansone.dev@gmail.com>
2024-06-23 21:58:40 +0200

    bump version

--- android/Omnivore/app/build.gradle.kts
+++ android/Omnivore/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "app.omnivore.omnivore"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2110000
-        versionName = "0.211.0"
+        versionCode = 2120000
+        versionName = "0.212.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
