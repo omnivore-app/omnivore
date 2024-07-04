a841f5748e6685a560eeab714b5b85d59fc9f7b3
Hongbo Wu <hongbo@omnivore.app>
2024-07-04 16:24:35 +0800

    map realistic voice to openai voice in tts-handler

--- packages/text-to-speech/src/openaiTextToSpeech.ts
+++ packages/text-to-speech/src/openaiTextToSpeech.ts
@@ -1,19 +1,82 @@
+import axios from 'axios'
+import { stripEmojis } from './htmlToSsml'
 import {
   TextToSpeech,
   TextToSpeechInput,
   TextToSpeechOutput,
 } from './textToSpeech'
-import axios from 'axios'
-import { stripEmojis } from './htmlToSsml'
 
-const OPEN_AI_VOICES = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
+const OPENAI_VOICE_PREFIX = 'openai-'
+
+const getVoiceId = (name: string | undefined): string | undefined => {
+  if (!name) {
+    return undefined
+  }
+
+  if (name.startsWith(OPENAI_VOICE_PREFIX)) {
+    return name.substring(OPENAI_VOICE_PREFIX.length)
+  }
+
+  // map realistic voice name to openai voice id
+  const voiceList = [
+    {
+      voiceId: 'ErXwobaYiN019PkySvjV',
+      name: 'echo',
+    },
+    {
+      voiceId: 'pMsXgVXv3BLzUgSXRplE',
+      name: 'alloy',
+    },
+    {
+      voiceId: 'onwK4e9ZLuTAKqWW03F9',
+      name: 'onyx',
+    },
+    {
+      voiceId: 'ThT5KcBeYPX3keUQqHPh',
+      name: 'fable',
+    },
+    {
+      voiceId: 'flq6f7yk4E4fJM5XTYuZ',
+      name: 'onyx',
+    },
+    {
+      voiceId: 'XrExE9yKIg1WjnnlVkGX',
+      name: 'shimmer',
+    },
+    {
+      voiceId: '21m00Tcm4TlvDq8ikWAM',
+      name: 'nova',
+    },
+    {
+      voiceId: 'EXAVITQu4vr4xnSDxMaL',
+      name: 'alloy',
+    },
+    {
+      voiceId: 'MF3mGyEYCl7XYWbV9V6O',
+      name: 'shimmer',
+    },
+    {
+      voiceId: 'TxGEqnHWrfWFTfGW9XjX',
+      name: 'echo',
+    },
+    {
+      voiceId: 'VR6AewLTigWG4xSOukaG',
+      name: 'nova',
+    },
+    {
+      voiceId: 'pNInz6obpgDQGcFmaJgB',
+      name: 'fable',
+    },
+  ]
+  return voiceList.find((voice) => voice.name === name)?.voiceId
+}
 
 export class OpenAITextToSpeech implements TextToSpeech {
   synthesizeTextToSpeech = async (
     input: TextToSpeechInput
   ): Promise<TextToSpeechOutput> => {
     const apiKey = process.env.OPENAI_API_KEY
-    const voice = input.voice?.substring('openai-'.length)
+    const voice = getVoiceId(input.voice)
 
     if (!apiKey) {
       throw new Error('API credentials not set')
@@ -51,6 +114,12 @@ export class OpenAITextToSpeech implements TextToSpeech {
     if (input.voice?.startsWith('openai-')) {
       return true
     }
+
+    // Use OpenAI voice for ultra realistic voice
+    if (input.isUltraRealisticVoice) {
+      return true
+    }
+
     return false
   }
 }
