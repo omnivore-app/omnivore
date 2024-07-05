ff80c324061eac154d4dadec008c09c091e4d200
Jackson Harper <jacksonh@gmail.com>
2024-07-05 18:11:38 +0800

    Fix python indentation

--- ml/digest-score/auth.py
+++ ml/digest-score/auth.py
@@ -4,7 +4,6 @@ from flask import request, jsonify
 from functools import wraps
 from datetime import datetime, timedelta
 
-
 SECRET_KEY = os.getenv('JWT_SECRET')
 ADMIN_SECRET_KEY = os.getenv('JWT_ADMIN_SECRET_KEY')
 
@@ -20,39 +19,38 @@ def generate_admin_token():
 
 
 def user_token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'Authorization' in request.headers:
-            print("request.headers['Authorization'].split(" ")[1]", request.headers['Authorization'].split(" ")[1])
-            token = request.headers['Authorization'].split(" ")[1]
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-        try:
-            data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-            request.user_id = data['uid']
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token has expired!'}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Token is invalid!'}), 401
-        return f(*args, **kwargs)
-    return decorated
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    token = None
+    if 'Authorization' in request.headers:
+      token = request.headers['Authorization'].split(" ")[1]
+    if not token:
+      return jsonify({'message': 'Token is missing!'}), 401
+    try:
+      data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+      request.user_id = data['uid']
+    except jwt.ExpiredSignatureError:
+      return jsonify({'message': 'Token has expired!'}), 401
+    except jwt.InvalidTokenError:
+      return jsonify({'message': 'Token is invalid!'}), 401
+    return f(*args, **kwargs)
+  return decorated
 
 def admin_token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'Authorization' in request.headers:
-            token = request.headers['Authorization'].split(" ")[1]
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-        try:
-            data = jwt.decode(token, ADMIN_SECRET_KEY, algorithms=["HS256"])
-            if data['role'] != 'admin':
-                return jsonify({'message': 'Admin token required!'}), 403
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token has expired!'}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Token is invalid!'}), 401
-        return f(*args, **kwargs)
-    return decorated
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    token = None
+    if 'Authorization' in request.headers:
+      token = request.headers['Authorization'].split(" ")[1]
+    if not token:
+      return jsonify({'message': 'Token is missing!'}), 401
+    try:
+      data = jwt.decode(token, ADMIN_SECRET_KEY, algorithms=["HS256"])
+      if data['role'] != 'admin':
+          return jsonify({'message': 'Admin token required!'}), 403
+    except jwt.ExpiredSignatureError:
+      return jsonify({'message': 'Token has expired!'}), 401
+    except jwt.InvalidTokenError:
+      return jsonify({'message': 'Token is invalid!'}), 401
+    return f(*args, **kwargs)
+  return decorated
