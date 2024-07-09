33721a0af81c4cf4072984e32e83af53fb96f14b
Jackson Harper <jacksonh@gmail.com>
2024-07-09 16:48:05 +0800

    Add a fallback to library if Home is not created for user
    Also update the hover stroke colour on the action icons.

--- packages/web/components/elements/icons/home/ShareActionIcon.tsx
+++ packages/web/components/elements/icons/home/ShareActionIcon.tsx
@@ -6,8 +6,6 @@ import React from 'react'
 
 export class ShareActionIcon extends React.Component<IconProps> {
   render() {
-    const strokeColor = (this.props.color || '#D9D9D9').toString()
-
     return (
       <svg
         width="19"
@@ -19,7 +17,7 @@ export class ShareActionIcon extends React.Component<IconProps> {
         <g>
           <path
             d="M10.2918 3.1665V6.33317C5.08661 7.147 3.15098 11.707 2.37515 15.8332C2.34586 15.9963 6.63748 11.1133 10.2918 11.0832V14.2498L16.6251 8.70817L10.2918 3.1665Z"
-            stroke={strokeColor}
+            stroke="var(--stroke)"
             strokeWidth="1.25"
             strokeLinecap="round"
             strokeLinejoin="round"
