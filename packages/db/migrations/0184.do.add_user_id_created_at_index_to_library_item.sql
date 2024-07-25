c503b47ea2e78ef1b9e1f96433f54ad80249bd6e
Hongbo Wu <hongbo@omnivore.app>
2024-07-25 12:10:53 +0800

    revert index

--- packages/db/migrations/0184.do.add_user_id_created_at_index_to_library_item.sql
+++
@@ -1,5 +0,0 @@
--- Type: DO
--- Name: add_user_id_created_at_index_to_library_item
--- Description: Add an index of columns user_id and created_at to the library_item table for counting query
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS library_item_user_id_created_at_idx ON omnivore.library_item (user_id, created_at);
