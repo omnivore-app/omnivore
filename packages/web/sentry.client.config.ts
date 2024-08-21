3299e1e67517e81be1e0a912f427d0f599fce640
Jackson Harper <jacksonh@gmail.com>
2024-08-21 21:34:49 +0800

    Fix sentry config

--- packages/web/sentry.client.config.ts
+++ packages/web/sentry.client.config.ts
@@ -14,19 +14,5 @@ if (sentryDSN) {
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
-    replaysOnErrorSampleRate: 1.0,
-
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
-
-    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-    integrations: [
-      Sentry.replayIntegration({
-        // Additional Replay configuration goes in here, for example:
-        maskAllText: true,
-        blockAllMedia: true,
-      }),
-    ],
   })
 }
